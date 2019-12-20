a0c924207cce5c3a10deafc8db6d928860f33eac
SeriousGuy888 <seriousguy888@users.noreply.github.com>
2019-12-20 12:43:11 -0500

    sdfg

--- commands/cult.js
+++ commands/cult.js
@@ -2,7 +2,11 @@ exports.run = async (client, message, args) => {
   const index = require("../index.js")
   const Discord = index.Discord
   
-  var cultCache = index.cultCache
+  let doc = db.collection("channels").doc("cult");
+  let observer = doc.onSnapshot(docSnapshot => {
+    console.log(`Cult channel change detected: ${docSnapshot._fieldsProto.id.stringValue}`)
+    index.cultCache = docSnapshot._fieldsProto.id.stringValue
+  }, err => {return})
 
-  return message.channel.send(cultCache)
+  return message.channel.send(index.cultCache)
 }
