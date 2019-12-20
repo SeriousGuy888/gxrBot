ce4805693168ac3407ef55e51854eb5c1d1898e7
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2019-12-20 10:39:56 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,8 +3,21 @@ exports.run = async (client, message, args) => {
   const fs = index.fs
   const Discord = index.Discord
 
-  if(!args[0]) {
+  function noArgs() {
     let helpHome = fs.readFileSync("./data/help.txt", "utf8")
     message.channel.send(helpHome)
+  }
+  
+  if(!args[0]) noArgs()
+  else {
+    switch(args[0]) {
+      case "cmds":
+        message.channel.send("test")
+        break
+      default:
+        message.channel.send("Invalid arguments.")
+        noArgs()
+        break
+    }
   }
 }
