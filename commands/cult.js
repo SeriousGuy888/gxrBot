b863c1c045a9706bd8a3134cbca461dfe05d3eb2
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2019-12-22 13:00:56 -0500

    changed cult cache system,
    can now cache the word as well as the chnanel id

--- commands/cult.js
+++ commands/cult.js
@@ -2,5 +2,5 @@ exports.run = async (client, message, args) => {
   const index = require("../index.js")
   const Discord = index.Discord
 
-  message.channel.send(`Cult channel is <#${index.cultCache}>\nThis command is currently work in progress. There will be a feature to censor all instances of free speech using this feature.`)
+  message.channel.send(`Cult channel is <#${index.cultCache.id}> and word is ${index.cultCache.word}\nThis command is currently work in progress. There will be a feature to censor all instances of free speech using this feature.`)
 }
