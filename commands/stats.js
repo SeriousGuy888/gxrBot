b875d8e6eb8abdca9e004426d3fb008aff5203ac
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2019-12-24 23:15:18 -0500

    Now tests to see if there are mentions

--- commands/stats.js
+++ commands/stats.js
@@ -4,7 +4,10 @@ exports.run = async (client, message, args) => {
   let queryUserId
 
   if(!args[0]) queryUserId = message.author.id
-  else if(args[0]) queryUserId = message.mentions.users.first().id || args[0]
+  else if(args[0]) {
+    if(!message.mentions.users.first()) queryUserId = args[0]
+    else queryUserId = message.mentions.users.first().id
+  }
 
   message.channel.send(queryUserId)
 }
