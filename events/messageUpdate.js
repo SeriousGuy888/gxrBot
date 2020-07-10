592ac675343e607facaa3efb056ad99ef78c539b
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-07-10 12:59:53 -0400

    you can no longer edit messages to bypass the league and ows restrictions

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,9 +1,5 @@
 module.exports = (client, oldMessage, newMessage) => {
   const index = require("../index.js")
-  const config = index.config
-  const prefix = index.prefix
-
-  const db = index.db
 
   let cultCache = index.cultCache
   let cultChannelId = cultCache.id.slice(2, cultCache.id.length - 1) // remove <# and > from channel mention to get id
@@ -12,22 +8,9 @@ module.exports = (client, oldMessage, newMessage) => {
   let owsCache = index.owsCache
   let owsChannelId = owsCache.id.slice(2, owsCache.id.length - 1) // remove <# and > from channel mention to get id
 
-  console.log(client)
-  console.log(oldMessage)
-  // console.log(newMessage)
-
   switch(oldMessage.channel.id) {
     case cultChannelId:
-      function messageLegal(msg, phrase) {
-        if(!msg || !phrase) return console.log("error with cult code in messageUpdate.js event")
-  
-        msg = msg.toLowerCase()
-        phrase = phrase.toLowerCase()
-  
-        if(msg == phrase) return true
-      }
-  
-      if(messageLegal(newMessage.content, cultPhrase) || newMessage.author.id == client.user.id) {
+      if(newMessage.content.toLowerCase() == cultPhrase || oldMessage.author.id == client.user.id) {
         return
       }
       else newMessage.delete().then(() => {
@@ -35,22 +18,15 @@ module.exports = (client, oldMessage, newMessage) => {
           return
         }
         
-        newMessage.channel.send(`<@${message.author.id}>, editing your message to say \`${message.content}\` is a violation of the cult rules.\nYou may only say \`${cultPhrase}\` here.`).then(msg => {
+        newMessage.channel.send(`<@${newMessage.author.id}>, editing your message to say \`${newMessage.content}\` is a violation of the cult rules.\nYou may only say \`${cultPhrase}\` here.`).then(msg => {
           msg.delete(3000) // delete message in 3 seconds
-        }).catch(err => {})
+        }).catch(err => {
+          console.log(err)
+        })
       })
       break
     case owsChannelId:
-      function messageLegal(msg) {
-        if(!msg) return console.log("error with ows code in messageUpdate.js event")
-  
-        msg = msg.toLowerCase().replace(/[^a-z ]/gi, "")
-        
-        if(msg.split(" ").length == 1) return true
-        else return false
-      }
-  
-      if(messageLegal(newMessage.content) || newMessage.author.id == client.user.id) {
+      if(newMessage.content.split(" ").length == 1 || newMessage.author.id == client.user.id) {
         return
       }
   
