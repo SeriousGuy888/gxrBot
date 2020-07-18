c508efe61acb525deb234c88711c891c6afcd869
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-07-18 17:35:38 -0400

    made the randArrElem function in billzo.js more concise

--- commands/billzo.js
+++ commands/billzo.js
@@ -3,6 +3,6 @@ exports.run = async (client, message, args) => {
   const adjectives = require("../data/billzo/adjectives.json")
   const nouns = require("../data/billzo/nouns.json")
 
-  const randArrElem = (arr) => arr[Math.floor(Math.random() * arr.length)]
+  const randArrElem = arr => arr[Math.floor(Math.random() * arr.length)]
   message.channel.send(`billzo the ${randArrElem(adjectives)} ${randArrElem(nouns)}`)
 }
