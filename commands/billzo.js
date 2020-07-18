0c821707a8573a9a14c3e24a23ac64a12d3eae88
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-07-18 17:19:09 -0400

    removed an unneeded Discord import in the billzo command

--- commands/billzo.js
+++ commands/billzo.js
@@ -1,11 +1,8 @@
 exports.run = async (client, message, args) => {
   const index = require("../index.js")
-  const Discord = index.Discord
-
   const adjectives = require("../data/billzo/adjectives.json")
   const nouns = require("../data/billzo/nouns.json")
 
   const randArrElem = (arr) => arr[Math.floor(Math.random() * arr.length)]
-
   message.channel.send(`billzo the ${randArrElem(adjectives)} ${randArrElem(nouns)}`)
 }
