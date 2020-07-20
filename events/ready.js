3237e37fd8d9c0e36a3e7ea24334a4ea007e36ca
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-07-19 22:55:08 -0400

    Revert "updated ready.js to rotate the status"
    This reverts commit f7c895d2107768f8d8c6c7cb9738984d523cd064.

--- events/ready.js
+++ events/ready.js
@@ -3,14 +3,5 @@ module.exports = (client, message) => {
   const prefix = index.prefix
 
   console.log("g9lBot Loaded!")
-
-  const statuses = [
-    `for ${prefix}help`,
-    "Hi, I'm InsomniCheez Lite!"
-  ]
-
-  setInterval(() => {
-    const index = Math.floor(Math.random() * (activities_list.length - 1) + 1)
-    client.user.setActivity(activities_list[index], { type: "WATCHING" })
-  }, 10000)
+  client.user.setActivity(`for ${prefix}help`, { type: "WATCHING" })
 }
