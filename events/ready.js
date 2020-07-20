b9930d409e4d400c55586088548f9ad8f7e3a3a8
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-07-19 22:54:54 -0400

    updated ready.js to rotate the status

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,14 @@ module.exports = (client, message) => {
   const prefix = index.prefix
 
   console.log("g9lBot Loaded!")
-  client.user.setActivity(`for ${prefix}help`, { type: "WATCHING" })
+
+  const statuses = [
+    `for ${prefix}help`,
+    "Hi, I'm InsomniCheez Lite!"
+  ]
+
+  setInterval(() => {
+    const index = Math.floor(Math.random() * (activities_list.length - 1) + 1)
+    client.user.setActivity(activities_list[index], { type: "WATCHING" })
+  }, 10000)
 }
