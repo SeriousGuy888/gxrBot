acfb18ceedaf5db146a59b05125e6871b1689ae3
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-08-04 17:00:41 -0400

    fixing help command

--- commands/help.js
+++ commands/help.js
@@ -3,16 +3,16 @@ exports.run = async (client, message, args) => {
   const fs = index.fs
 
   function noArgs() {
-    let helpHome = fs.readFileSync("./data/help/help.txt", "utf8")
+    let helpHome = readFile("help")
     message.channel.send(helpHome)
   }
   
   function readFile(name) {
-    if(!fs.existsSync(`./data/help/${args[0]}.txt`)) return `Error: File \`${name}\` does not exist.`
+    if(!fs.existsSync(`./data/help/${name}.txt`)) return `Error: File \`${name}\` does not exist.`
     let fileContents = fs.readFileSync(`./data/help/${name}.txt`, "utf8")
-    return fileContents
+    return fileContents.replace(/%prefix%/gi, index.config.prefix)
   }
   
   if(!args[0]) noArgs()
-  else message.channel.send(readFile(args[0]).replace(/%prefix%/gi, index.config.prefix))
+  else message.channel.send(readFile(args[0]))
 }
