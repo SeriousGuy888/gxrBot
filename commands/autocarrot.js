dd857227f1c072428352e49e29e575d9e4cdc234
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-08-04 17:36:36 -0400

    disabled autocarrot command

--- commands/autocarrot.js
+++ commands/autocarrot.js
@@ -2,6 +2,6 @@ exports.run = async (client, message, args) => {
   const index = require("../index.js")
   const autocarrotWebhook = index.autocarrotWebhook
   
-  autocarrotWebhook(message.author, message.channel, args.join(" "))
-  // message.delete()
+  return message.channel.send("Command disabled.")
+  // autocarrotWebhook(message.author, message.channel, args.join(" "))
 }
