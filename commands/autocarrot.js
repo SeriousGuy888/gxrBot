10cf51b349f6c75b7a10cfb8bd3b164369327ed2
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-08-06 00:32:12 -0400

    redid the config read thing for autocarrot

--- commands/autocarrot.js
+++ commands/autocarrot.js
@@ -2,7 +2,7 @@ exports.run = async (client, message, args) => {
   const index = require("../index.js")
   const autocarrotWebhook = index.autocarrotWebhook
   
-  message.channel.send("Current autocarrot config. I'm too lazy to format it so it's raw JSON. ```" + JSON.stringify(index.config.autocarrot) + "```")
+  message.channel.send("Current autocarrot config in raw JSON :D\n```json" + JSON.stringify(index.config.autocarrot, null, 2) + "```")
 
   // return message.channel.send("Command disabled.")
   // autocarrotWebhook(message.author, message.channel, args.join(" "))
