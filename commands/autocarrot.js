64bd32d56c244ba436edf109f20986b02f980fae
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-08-06 00:23:58 -0400

    autocarrot command now replies with current autocarrot config

--- commands/autocarrot.js
+++ commands/autocarrot.js
@@ -2,6 +2,8 @@ exports.run = async (client, message, args) => {
   const index = require("../index.js")
   const autocarrotWebhook = index.autocarrotWebhook
   
-  return message.channel.send("Command disabled.")
+  message.channel.send("Current autocarrot config. I'm too lazy to format it so it's raw JSON. ```" + JSON.stringify(index.config.autocarrot) + "```")
+
+  // return message.channel.send("Command disabled.")
   // autocarrotWebhook(message.author, message.channel, args.join(" "))
 }
