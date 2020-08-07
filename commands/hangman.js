7aa34ba123e4faaa4b38c5443791956e11bdb0a8
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-08-07 14:38:48 -0400

    hangman command now interacts with the game cache
    the user will be stored in the object when they start a game. they will also be prevented from starting a new game when they are already stored in the cache.
    
    what if the bot spontaneously restarts?
    answer: i have no idea

--- commands/hangman.js
+++ commands/hangman.js
@@ -1,20 +1,26 @@
 exports.run = async (client, message, args) => {
   const index = require("../index.js")
   const Discord = index.Discord
+  const hangmanCache = index.gameCache.hangman
 
-  message.channel.send("idk what im doing")
+  if(hangmanCache[message.author.id]) return message.channel.send("you are already playing a game of hangman probably")
 
   const collectorTimeout = 30
   const cancelEmoji = "❌"
+
+  hangmanCache[message.author.id] = {
+    word: "quack",
+    guesses: -1,
+    stupid: true
+  }
+  message.channel.send(JSON.stringify(hangmanCache, null, 2))
 
   const gameEmbed = new Discord.RichEmbed()
     .setTitle("guess the word or this human gets executed")
     .setDescription("aifhsjldfgujdofgidfgll/ll")
   const msg = await message.channel.send(gameEmbed)
+
   await msg.react(cancelEmoji)
-  // await msg.react(b)
-  // await msg.react(c)
-  // await msg.react(d)
   
   const filter = (reaction, user) => reaction.emoji.name == cancelEmoji && user.id == message.author.id
   msg.awaitReactions(filter, {
@@ -31,5 +37,7 @@ exports.run = async (client, message, args) => {
     msg.edit("time expired or maybe you chose an invalid option", {
       embed: {}
     })
+    hangmanCache[message.author.id] = undefined
+    delete hangmanCache[message.author.id]
   })
 }
