5bd4636905248a17c4d31193bfcb39188101b1ba
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-08-07 14:25:34 -0400

    changed some hangman stuff

--- commands/hangman.js
+++ commands/hangman.js
@@ -5,18 +5,18 @@ exports.run = async (client, message, args) => {
   message.channel.send("idk what im doing")
 
   const collectorTimeout = 30
-  const confirmationEmoji = "✅"
+  const cancelEmoji = "❌"
 
   const gameEmbed = new Discord.RichEmbed()
     .setTitle("guess the word or this human gets executed")
     .setDescription("aifhsjldfgujdofgidfgll/ll")
   const msg = await message.channel.send(gameEmbed)
-  await msg.react(confirmationEmoji)
+  await msg.react(cancelEmoji)
   // await msg.react(b)
   // await msg.react(c)
   // await msg.react(d)
   
-  const filter = (reaction, user) => reaction.emoji.name == confirmationEmoji && user.id == message.author.id
+  const filter = (reaction, user) => reaction.emoji.name == cancelEmoji && user.id == message.author.id
   msg.awaitReactions(filter, {
     max: 1,
     time: collectorTimeout * 1000,
@@ -24,9 +24,11 @@ exports.run = async (client, message, args) => {
   }).then(collected => {
     const reaction = collected.first()
 
-    if(reaction.emoji.name === confirmationEmoji) message.channel.send("Correct")
+    if(reaction.emoji.name === cancelEmoji) message.channel.send("Correct")
     else message.channel.send("Incorrect")
   }).catch(collected => {
-    msg.edit("time expired or maybe you chose an invalid option")
+    msg.edit("time expired or maybe you chose an invalid option", {
+      embed: {}
+    })
   })
 }
