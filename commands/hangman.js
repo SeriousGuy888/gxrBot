da5edc080f219ee76b4544f02a047c75e1ccc164
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-08-07 15:02:00 -0400

    adding a guessing system

--- commands/hangman.js
+++ commands/hangman.js
@@ -3,45 +3,56 @@ exports.run = async (client, message, args) => {
   const Discord = index.Discord
   const hangmanCache = index.gameCache.hangman
 
-  if(hangmanCache[message.author.id]) return message.channel.send("you are already playing a game of hangman probably")
+  if(args[0] == "guess") {
+    if(!hangmanCache[message.author.id]) return message.channel.send("You are not currently playing hangman.")
+    if(!args[1]) return message.channel.send("Please specify a letter, you idiot.")
+    
+    const guessChar = args[1].charAt(0).toLowerCase()
+    if(!guessChar.match(/[a-z]/gi)) return message.channel.send("You have to guess a letter in the English Alphabet, idiot.")
 
-  const collectorTimeout = 30
-  const cancelEmoji = "❌"
-
-  const clearUserHangman = user => {
-    hangmanCache[user.id] = undefined
-    delete hangmanCache[user.id]
+    message.channel.send(guessChar)
   }
-
-  hangmanCache[message.author.id] = {
-    word: "quack",
-    guesses: 0,
-    attempedLetters: []
-  }
-  message.channel.send(JSON.stringify(hangmanCache, null, 2))
-
-  const gameEmbed = new Discord.RichEmbed()
-    .setColor("#ad3232")
-    .setTitle("guess the word or this human gets executed")
-    .setDescription("\\_ ".repeat(hangmanCache[message.author.id].word.length))
-  const msg = await message.channel.send(gameEmbed)
-
-  await msg.react(cancelEmoji)
+  else {
+    if(hangmanCache[message.author.id]) return message.channel.send("you are already playing a game of hangman probably")
   
-  const filter = (reaction, user) => reaction.emoji.name == cancelEmoji && user.id == message.author.id
-  msg.awaitReactions(filter, {
-    max: 1,
-    time: collectorTimeout * 1000,
-    errors: ["time"]
-  }).then(collected => {
-    const reaction = collected.first()
-
-    if(reaction.emoji.name === cancelEmoji) {
-      message.channel.send("cancelled?")
-      clearUserHangman(message.author)
+    const collectorTimeout = 30
+    const cancelEmoji = "❌"
+  
+    const clearUserHangman = user => {
+      hangmanCache[user.id] = undefined
+      delete hangmanCache[user.id]
     }
-  }).catch(collected => {
-    msg.edit("time expired or maybe you chose an invalid option", { embed: {} })
-    clearUserHangman(message.author)
-  })
+  
+    hangmanCache[message.author.id] = {
+      word: "quack",
+      guesses: 0,
+      attempedLetters: []
+    }
+    message.channel.send(JSON.stringify(hangmanCache, null, 2))
+  
+    const gameEmbed = new Discord.RichEmbed()
+      .setColor("#ad3232")
+      .setTitle("guess the word or this human gets executed")
+      .setDescription("\\_ ".repeat(hangmanCache[message.author.id].word.length))
+    const msg = await message.channel.send(gameEmbed)
+  
+    await msg.react(cancelEmoji)
+    
+    const filter = (reaction, user) => reaction.emoji.name == cancelEmoji && user.id == message.author.id
+    msg.awaitReactions(filter, {
+      max: 1,
+      time: collectorTimeout * 1000,
+      errors: ["time"]
+    }).then(collected => {
+      const reaction = collected.first()
+  
+      if(reaction.emoji.name === cancelEmoji) {
+        message.channel.send("cancelled?")
+        clearUserHangman(message.author)
+      }
+    }).catch(collected => {
+      msg.edit("time expired or maybe you chose an invalid option", { embed: {} })
+      clearUserHangman(message.author)
+    })
+  }
 }
