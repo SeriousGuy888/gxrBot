4fe003f20570e2f32f28871894a0f9ce9fd6e3d6
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-08-28 15:30:34 -0400

    main config is now _main because alphabetical sorting

--- config/_config.js
+++ config/_config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  main: require("./main.json"),
+  main: require("./_main.json"),
   admins: require("./admins.json"),
   autocarrot: require("./autocarrot.json"),
   autoResponses: require("./auto_responses.json"),
