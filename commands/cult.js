7387b8edeb3688f4010cd71e079d5ea7431abccf
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-08-30 23:23:45 -0400

    replace unnecessary switch/case with if/else

--- commands/cult.js
+++ commands/cult.js
@@ -19,34 +19,32 @@ exports.run = async (client, message, args) => {
   else if(args[0]) {
     if(!config.admin.ids[message.author.id]) return message.channel.send("You are not listed as an admin! (If this is inaccurate, contact billzo (contact billzo da magic programmer and not billzo da magic caterpillar))")
     
-    switch(args[0]) {
-      case "set":
-        if(!args[1]) return message.channel.send("Syntax: `c!cult set <channel | phrase> <params>`")
-        switch(args[1]) {
-          case "channel":
-            if(!args[2]) return message.channel.send("Syntax: `c!cult set channel <channel mention>`")
+    if(args[0] === "set") {
+      if(!args[1]) return message.channel.send("Syntax: `c!cult set <channel | phrase> <params>`")
+      switch(args[1]) {
+        case "channel":
+          if(!args[2]) return message.channel.send("Syntax: `c!cult set channel <channel mention>`")
 
-            doc.set({id: args[2]}, {merge: true}).then(() => {
-              message.channel.send("Channel set!")
-            })
+          doc.set({id: args[2]}, {merge: true}).then(() => {
+            message.channel.send("Channel set!")
+          })
 
-            break
-          case "phrase":
-            if(!args[2]) return message.channel.send("Syntax: `c!cult set phrase <words>`")
+          break
+        case "phrase":
+          if(!args[2]) return message.channel.send("Syntax: `c!cult set phrase <words>`")
 
-            doc.set({word: args.slice(2).join(" ")}, {merge: true}).then(() => {
-              message.channel.send("Phrase set!")
-            })
+          doc.set({word: args.slice(2).join(" ")}, {merge: true}).then(() => {
+            message.channel.send("Phrase set!")
+          })
 
-            break
-          default:
-            message.channel.send("Invalid arguments!")
-            break
-        }
-        break
-      default:
-        message.channel.send("Invalid arguments!")
-        break
+          break
+        default:
+          message.channel.send("Invalid arguments!")
+          break
+      }
+    }
+    else {
+      message.channel.send("Invalid arguments!")
     }
   }
 }
