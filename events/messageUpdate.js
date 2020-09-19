d8c4178c646b72d64a21ebd5fb89c528644d430e
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-09-19 18:33:01 -0400

    messageUpdate event no longer uses legacy cult code

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,24 +1,18 @@
+const { config } = require("../index.js")
+
 module.exports = (client, oldMessage, newMessage) => {
   const index = require("../index.js")
-
-  let cultCache = index.cultCache
-  let cultChannelId = cultCache.id.slice(2, cultCache.id.length - 1) // remove <# and > from channel mention to get id
-  let cultPhrase = cultCache.word
 
   let owsCache = index.owsCache
   let owsChannelId = owsCache.id.slice(2, owsCache.id.length - 1) // remove <# and > from channel mention to get id
 
   switch(oldMessage.channel.id) {
-    case cultChannelId:
-      if(newMessage.content.toLowerCase() == cultPhrase || oldMessage.author.id == client.user.id) {
-        return
-      }
+    case config.coopchannels.cult.channel:
+      if(newMessage.content.toLowerCase() == config.coopchannels.cult.phrase || oldMessage.author.id == client.user.id) return
       else newMessage.delete().then(() => {
-        if(newMessage.author.bot) {
-          return
-        }
+        if(newMessage.author.bot) return
         
-        newMessage.channel.send(`<@${newMessage.author.id}>, editing your message to say \`${newMessage.content}\` is a violation of the cult rules.\nYou may only say \`${cultPhrase}\` here.`).then(msg => {
+        newMessage.channel.send(`<@${newMessage.author.id}>, editing your message to say \`${newMessage.content}\` is a violation of the cult rules.\nYou may only say \`${config.coopchannels.cult.phrase}\` here.`).then(msg => {
           msg.delete(3000) // delete message in 3 seconds
         }).catch(err => {
           console.log(err)
