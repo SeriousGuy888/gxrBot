dfd81532119c85583fb439873d446e63ac066eab
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-09-19 12:36:24 -0400

    embedder not embedManager

--- commands/te.js
+++ commands/te.js
@@ -1,7 +1,7 @@
 exports.run = async (client, message, args) => {
   const embedder = client.util.get("embedder")
 
-  let emb = embedManager.newEmbed()
+  let emb = embedder.newEmbed()
     .setColor("#3333ee")
     .setTitle("lorem ipsum")
     .setDescription("dolor sit amet")
