9f367c4c6178b057acf75cc1b8f5773b4d31df4e
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-09-19 13:04:30 -0400

    i think this might return the message now

--- util/messenger.js
+++ util/messenger.js
@@ -2,9 +2,11 @@ const { client } = require("..")
 
 // * supposed to make stuff like sending dms easier. sending to a channel is not managed by this.
 exports.dm = (userId, content) => {
+  let message
   client.users.fetch(userId).then(user => {
-    user.send(content).then(message => message)
+    user.send(content).then(msg => message = msg)
   }).catch(err => {
     throw new Error(err)
   })
+  return message
 }
