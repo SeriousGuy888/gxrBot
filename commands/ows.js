a8b14a455bb91809b06943e39bc5c6221b28c01a
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-09-19 18:45:09 -0400

    ows command once again sends channel mention

--- commands/ows.js
+++ commands/ows.js
@@ -7,7 +7,7 @@ exports.run = async (client, message, args) => {
     .setColor("#aaaadd")
     .setTitle("OWS Info")
     .setDescription("*OWS channel options have been moved to the config due to infrequent use. You can no longer set them with this command.*")
-    .addField("Channel", `${config.coopchannels.ows.channel}`)
+    .addField("Channel", `<#${config.coopchannels.ows.channel}>`)
   
   message.channel.send(emb)
 }
