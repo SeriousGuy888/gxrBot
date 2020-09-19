0fad441bbf171249b0c94d6a157958516d94392f
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-09-19 13:18:00 -0400

    messenger callback is now optional

--- commands/test_dm.js
+++ commands/test_dm.js
@@ -10,7 +10,5 @@ exports.run = async (client, message, args) => {
   embedder.addBlankField(emb)
     .addField("def")
     
-  messenger.dm(args[0], emb, msg => {
-    msg.delete({ delay: 3000 })
-  })
+  messenger.dm(args[0], emb)
 }
