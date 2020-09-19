e37b4613ba932f5c48f1c4a8e0d5d7aba2a8a0e8
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-09-19 18:38:59 -0400

    one_word_story is no longer allowed to edit ows options

--- commands/one_word_story.js
+++ commands/one_word_story.js
@@ -2,38 +2,12 @@ exports.run = async (client, message, args) => {
   const index = require("../index.js")
   const config = index.config
   const Discord = index.Discord
-
-  const db = index.db
-
-  let doc = db.collection("channels").doc("one_word_story")
   
-  if(!args[0]) {
-    let emb = new Discord.MessageEmbed()
-      .setColor("#aaaadd")
-      .setTitle("OWS Info")
-      .addField("Channel", `${index.owsCache.id}`)
-    
-    message.channel.send(emb)
-  }
-  else if(args[0]) {
-    if(!config.admin.ids[message.author.id]) return message.channel.send("You are not listed as an admin! (If this is inaccurate, contact billzo (contact billzo da magic programmer and not billzo da magic caterpillar))")
-    
-    if(args[0] == "set") {
-      if(!args[1]) return message.channel.send("Syntax: `c!one_word_story set channel <channel mention>`")
-
-      if(args[1] == "channel") {
-        if(!args[2]) return message.channel.send("Syntax: `c!one_word_story set channel <channel mention>`")
-
-        doc.set({id: args[2]}, {merge: true}).then(() => {
-          message.channel.send("Channel set!")
-        })
-      }
-      else {
-        message.channel.send("Invalid arguments!")
-      }
-    }
-    else {
-      message.channel.send("Invalid arguments!")
-    }
-  }
+  let emb = new Discord.MessageEmbed()
+    .setColor("#aaaadd")
+    .setTitle("OWS Info")
+    .setDescription("*OWS channel options have been moved to the config due to infrequent use. You can no longer set them with this command.*")
+    .addField("Channel", `${index.owsCache.id}`)
+  
+  message.channel.send(emb)
 }
