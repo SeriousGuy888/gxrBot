e0811a1e745fddd024c0c1a96f09058f0bd6f5fb
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-09-19 18:49:35 -0400

    refactored messageUpdate event
    - removed switch/case where if/else made more sense
    
    - neatified some stuff that should have been single line

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,35 +1,24 @@
 const { config } = require("../index.js")
 
 module.exports = (client, oldMessage, newMessage) => {
-  switch(oldMessage.channel.id) {
-    case config.coopchannels.cult.channel:
-      if(newMessage.content.toLowerCase() == config.coopchannels.cult.phrase || oldMessage.author.id == client.user.id) return
-      else newMessage.delete().then(() => {
-        if(newMessage.author.bot) return
-        
-        newMessage.channel.send(`<@${newMessage.author.id}>, editing your message to say \`${newMessage.content}\` is a violation of the cult rules.\nYou may only say \`${config.coopchannels.cult.phrase}\` here.`).then(msg => {
-          msg.delete(3000) // delete message in 3 seconds
-        }).catch(err => {
-          console.log(err)
-        })
-      })
-      break
-    case config.coopchannels.ows.channel:
-      if(newMessage.content.split(" ").length == 1 || newMessage.author.id == client.user.id) {
-        return
-      }
-  
-      else newMessage.delete().then(() => {
-        if(newMessage.author.bot) {
-          return
-        }
-        
-        newMessage.channel.send(`<@${message.author.id}>, this is the one word story channel. You are a stupid.`).then(msg => {
-          msg.delete(3000) // delete message in 3 seconds
-        }).catch(err => {})
-      })
-      break
-    default:
-      break
+  if(config.coopchannels.cult.channel == oldMessage.channel.id) {
+    if(newMessage.content.toLowerCase() == config.coopchannels.cult.phrase || oldMessage.author.id == client.user.id) return
+    newMessage.delete().then(() => {
+      if(newMessage.author.bot) return
+      
+      newMessage.channel.send(`<@${newMessage.author.id}>, editing your message to say \`${newMessage.content}\` is a violation of the cult rules.\nYou may only say \`${config.coopchannels.cult.phrase}\` here.`)
+        .then(msg => msg.delete(3000))
+        .catch(console.error)
+    })
+  }
+  else if(config.coopchannels.ows.channel == oldMessage.channel.id) {
+    if(newMessage.content.split(" ").length == 1 || newMessage.author.id == client.user.id) return
+    newMessage.delete().then(() => {
+      if(newMessage.author.bot) return
+      
+      newMessage.channel.send(`<@${message.author.id}>, this is the one word story channel. You are a stupid.`)
+        .then(msg => msg.delete(3000))
+        .catch(console.error)
+    })
   }
 }
