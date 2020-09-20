1e6e4b8cb816c1376bd098bd88602096a9055d28
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-09-20 00:26:23 -0400

    reaction emoji name?

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,5 +1,5 @@
 module.exports = (client, reaction, user) => {
   const { message } = reaction
   if(message.channel.id != "757091932395798549") return
-  message.channel.send(reaction.emoji)
+  message.channel.send(`${reaction.emoji} ${reaction.emoji.name}`)
 }
