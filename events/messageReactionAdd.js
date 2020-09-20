986349a014dee99eae2cfeb72c375892ed755def
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-09-20 00:35:55 -0400

    disabled messageReactionAdd

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,7 +1,9 @@
 module.exports = async (client, reaction, user) => {
+  return
   if(reaction.message.partial) {
     const message = await reaction.message.fetch()
     if(message.channel.id != "757091932395798549") return
     message.channel.send(`${reaction.emoji} ${reaction.emoji.name}`)
+    // todo: handle when the message is not partial
   }
 }
