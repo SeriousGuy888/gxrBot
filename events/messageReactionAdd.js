b02d5599794611e6b8c39f01884f9578e11e421c
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-09-20 15:48:41 -0400

    minor changes

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,9 +1,8 @@
 module.exports = async (client, reaction, user) => {
-  return
-  if(reaction.message.partial) {
-    const message = await reaction.message.fetch()
-    if(message.channel.id != "757091932395798549") return
-    message.channel.send(`${reaction.emoji} ${reaction.emoji.name}`)
-    // todo: handle when the message is not partial
-  }
+  let message = reaction.message
+  if(message.channel.id != "757091932395798549") return
+  if(reaction.message.partial) message = await reaction.message.fetch()
+  
+  let a = reaction.count
+  message.channel.send(`${a}`)
 }
