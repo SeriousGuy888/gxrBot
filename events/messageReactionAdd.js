7638d5f29ab76d2287bf068dc9c453b8f18aa610
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-09-20 00:28:48 -0400

    fetching message for reactoinas

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,5 +1,7 @@
-module.exports = (client, reaction, user) => {
-  const { message } = reaction
-  if(message.channel.id != "757091932395798549") return
-  message.channel.send(`${reaction.emoji} ${reaction.emoji.name}`)
+module.exports = async (client, reaction, user) => {
+  if(reaction.message.partial) {
+    const message = await reaction.message.fetch()
+    if(message.channel.id != "757091932395798549") return
+    message.channel.send(`${reaction.emoji} ${reaction.emoji.name}`)
+  }
 }
