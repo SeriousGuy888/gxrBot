2a59e6bb5a198aea47b24bb1275c55796a55292c
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-09-22 15:07:13 -0400

    cult command embed colour now in config

--- commands/cult.js
+++ commands/cult.js
@@ -3,12 +3,14 @@ exports.run = async (client, message, args) => {
   const config = index.config
   const Discord = index.Discord
   
+  const cultConfig = config.coopchannels.cult
+
   let emb = new Discord.MessageEmbed()
-    .setColor("#aaaadd")
+    .setColor(cultConfig.embedColour)
     .setTitle("Cult Activities Channel Information")
     .setDescription("*Cult channel options have been moved to the config due to infrequent use. You can no longer set them with this command.*")
-    .addField("Channel", `<#${config.coopchannels.cult.channel}>`)
-    .addField("Phrase", config.coopchannels.cult.phrase)
+    .addField("Channel", `<#${cultConfig.channel}>`)
+    .addField("Phrase", cultConfig.phrase)
   
   message.channel.send(emb)
 }
