0944aef0a69c75ce5dbe2dfb77a9705491628d45
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-09-22 16:34:10 -0400

    i dont feel like fixing this right now

--- commands/_dev/test_dm.js
+++ commands/_dev/test_dm.js
@@ -1,14 +1,15 @@
 exports.run = async (client, message, args) => {
-  const embedder = client.util.get("embedder")
-  const messenger = client.util.get("messenger")
+  return
+  // const embedder = client.util.get("embedder")
+  // const messenger = client.util.get("messenger")
 
-  let emb = embedder.newEmbed()
-    .setColor("#3333ee")
-    .setTitle("lorem ipsum")
-    .setDescription("dolor sit amet")
-    .addField("abc")
-  embedder.addBlankField(emb)
-    .addField("def")
+  // let emb = embedder.newEmbed()
+  //   .setColor("#3333ee")
+  //   .setTitle("lorem ipsum")
+  //   .setDescription("dolor sit amet")
+  //   .addField("abc")
+  // embedder.addBlankField(emb)
+  //   .addField("def")
     
-  messenger.dm(message.author.id, emb)
+  // messenger.dm(message.author.id, "a")
 }
