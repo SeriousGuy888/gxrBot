29d6dd8074f7831e0b419eaa4d1bde07233bfc69
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-09-22 17:08:33 -0400

    fixed ping command imports

--- commands/util/ping.js
+++ commands/util/ping.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args) => {
-  const index = require("../index.js")
+  const index = require("../../index.js")
   const Discord = index.Discord
 
   const timeFormatter = index.timeFormatter
