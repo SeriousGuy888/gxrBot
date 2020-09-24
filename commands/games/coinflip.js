7fa8073ab79c265516924117636dbb46d8e76e35
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-09-23 20:15:12 -0400

    coin flip can now land on rim

--- commands/games/coinflip.js
+++ commands/games/coinflip.js
@@ -2,13 +2,16 @@ exports.run = async (client, message, args) => {
   const index = require("../../index.js")
   const Discord = index.Discord
 
+  const randomNumber = Math.floor(Math.random() * 3) + 1
+  const rim = randomNumber === 1
   const flipResult = !!Math.round(Math.random())
-  const attachment = new Discord.MessageAttachment(`./assets/coinflip/${flipResult ? "heads" : "tails"}.png`, "coin.png")
+
+  const attachment = new Discord.MessageAttachment(`./assets/coinflip/${rim ? "rim" : flipResult ? "heads" : "tails"}.png`, "coin.png")
   const emb = new Discord.MessageEmbed()
     .attachFiles(attachment)
     .setImage("attachment://coin.png")
     .setColor("#a2a212")
-    .setTitle(flipResult ? "Heads" : "Tails")
+    .setTitle(rim ? "Rim" : flipResult ? "Heads" : "Tails")
 
   message.channel.send(emb)
 }
