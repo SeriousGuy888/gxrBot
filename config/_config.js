21c860dce7b83ed6dcb38a15c1a0f75af5baf191
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-09-27 00:25:27 -0400

    reordered _config
    directories on top and files on the bottom, both individually in alphabetical order

--- config/_config.js
+++ config/_config.js
@@ -1,11 +1,11 @@
 module.exports = {
+  autocarrot:      require("./autocarrot/_autocarrot.js"),
+  billzo:          require("./billzo/_billzo.js"),
+  coopchannels:    require("./coopchannels/_coopchannels.js"),
+  hangman:         require("./hangman/_hangman.js"),
+  help:            require("./help/_help.js"),
   main:            require("./_main.json"),
   admins:          require("./admins.json"),
-  autocarrot:      require("./autocarrot/_autocarrot.js"),
   autoResponses:   require("./auto_responses.json"),
-  hangman:         require("./hangman/_hangman.js"),
-  billzo:          require("./billzo/_billzo.js"),
-  help:            require("./help/_help.js"),
-  coopchannels:    require("./coopchannels/_coopchannels.js"),
   mute:            require("./mute.json")
 }
