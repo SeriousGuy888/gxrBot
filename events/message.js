ea660124e9a3c3d924b672f5de72d9099ef28e1c
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-10-24 23:03:05 -0400

    Revert "Revert "bots no longer exempt from autocarrot""
    This reverts commit 7bd1740ba6030f7a7bb0cfdeb539d1d94113dd63.

--- events/message.js
+++ events/message.js
@@ -5,16 +5,13 @@ module.exports = (client, message) => {
   
   const messenger = client.util.get("messenger")
 
-  let pauseAutocarrotCache = index.pauseAutocarrotCache
-
-  let args
-  let command
-  let cmd
-
-
-  if(message.author.bot) return //ignore bots
-
   commands:
+    if(message.author.bot) return
+
+    let args
+    let command
+    let cmd
+
     if(message.content.toLowerCase().indexOf(prefix) === 0) {
 
       args = message.content.slice(prefix.length).trim().split(/ +/g)
@@ -44,6 +41,8 @@ module.exports = (client, message) => {
     }
 
   autocarrot:
+    let pauseAutocarrotCache = index.pauseAutocarrotCache
+
     if(config.autocarrot.settings.enabled) {
       if(message.content.toLowerCase().includes(config.autocarrot.settings.pause.message)) {
         pauseAutocarrotCache[message.author.id] = {
@@ -80,9 +79,16 @@ module.exports = (client, message) => {
       }
     }
 
-
-  if([config.coopchannels.cult.channel, config.coopchannels.ows.channel].includes(message.channel.id)) index.coopChannels(message)
+  coopChannels:
+    if([config.coopchannels.cult.channel, config.coopchannels.ows.channel].includes(message.channel.id))
+      index.coopChannels(message)
   
-  if(config.autoResponses.settings.enabled) index.autoResponses(message)
-  index.pog(message)
+  autoResponses:
+    if(message.author.bot) return
+    if(config.autoResponses.settings.enabled)
+      index.autoResponses(message)
+  
+  pog:
+    if(message.author.bot) return
+    index.pog(message)
 }
