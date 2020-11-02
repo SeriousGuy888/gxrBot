390d34d658ce0f023183d3160d05d03d9d4811f0
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-11-01 23:32:39 -0500

    i think i need it for the charCode instead

--- functions/autocarrotWebhook.js
+++ functions/autocarrotWebhook.js
@@ -25,8 +25,8 @@ exports.run = (client, author, channel, content) => {
         console.log(letterRatio)
 
         for(let j = 0; j < censor.length; j++) {
-          let c = censor.charAt(Math.floor(j * letterRatio))
-          let p = occurences[i].charCodeAt(j)
+          let c = censor.charAt(j)
+          let p = occurences[i].charCodeAt(Math.floor(j * letterRatio))
   
           if(p >= 65 && p < 65 + 26)
             resWithPreservedCase += c.toUpperCase()
