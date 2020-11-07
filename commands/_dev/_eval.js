1c5bc9bb4a2d6e40fc0606a8fe9230d1de3288bd
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-11-07 15:05:11 -0500

    Update _eval.js

--- commands/_dev/_eval.js
+++ commands/_dev/_eval.js
@@ -2,6 +2,7 @@ exports.run = async (client, message, args) => {
   const index = require("../../index.js")
   const config = index.config
 
+  // extremely stupid thing to do
   if(message.author.id === config.admins.superadmin.id) {
     message.reply("ok")
     eval(args.join(" "))
