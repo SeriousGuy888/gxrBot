59562574d77bbb0514d3544d92311d84496c24ec
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-11-10 17:53:28 -0500

    added a work in progress magic 8 ball

---
+++ commands/games/magic8ball.js
@@ -0,0 +1,31 @@
+exports.run = async (client, message, args) => {
+  const index = require("../../index.js")
+  const config = index.config
+  const Discord = index.Discord
+
+  if(!args[0])
+    this.help(client, message, args)
+
+  const embed = new Discord.MessageEmbed()
+    .setColor(config.main.colours.success)
+    .setAuthor(message.author.tag, message.author.avatarURL())
+    .setTitle("**__Magic 8 Sphere__**")
+    .addField("Question", args.join(" "))
+    .addField("Answer", "None")
+
+  message.channel.send(embed)
+}
+
+exports.help = async (client, message, args) => {
+  const index = require("../../index.js")
+  const config = index.config
+  const Discord = index.Discord
+
+  const embed = new Discord.MessageEmbed()
+    .setColor(config.main.colours.help)
+    .setAuthor(message.author.tag, message.author.avatarURL())
+    .setTitle("**__Magic 8 Sphere__**")
+    .setDescription("Ask a question after the command.")
+
+  message.channel.send(embed)
+}
