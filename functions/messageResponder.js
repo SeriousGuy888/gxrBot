b12b974720bac2a9c67b701c5dfa8e2ab5a80d26
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-11-13 15:07:18 -0500

    added a thing to messageResponder

--- functions/messageResponder.js
+++ functions/messageResponder.js
@@ -5,6 +5,8 @@ exports.run = (client, message) => { // super secret feature that will get remov
 
   if(message.content.match(/pog/gi) && !message.content.match(/(un|not )(pog)/gi))
     response = "pog"
+  if(message.content.match(/i'?m (lonely|sad)/gi))
+    response = "D: don't be"
 
   if(response)
     message.channel.send(response)
