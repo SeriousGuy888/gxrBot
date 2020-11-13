a92f7935de7bf11dd04280c68bf203032a7b05e1
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-11-13 15:17:57 -0500

    added messageResponder config

--- config/_config.js
+++ config/_config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  autocarrot:      require("./autocarrot/_autocarrot.js"),
-  autoResponses:   require("./auto_responses/_auto_responses.js"),
-  billzo:          require("./billzo/_billzo.js"),
-  coopchannels:    require("./coopchannels/_coopchannels.js"),
-  hangman:         require("./hangman/_hangman.js"),
-  help:            require("./help/_help.js"),
-  main:            require("./_main.json"),
-  admins:          require("./admins.json"),
-  magic8ball:      require("./magic8ball.json"),
-  mute:            require("./mute.json")
+  autocarrot:         require("./autocarrot/_autocarrot.js"),
+  autoResponses:      require("./auto_responses/_auto_responses.js"),
+  billzo:             require("./billzo/_billzo.js"),
+  coopchannels:       require("./coopchannels/_coopchannels.js"),
+  hangman:            require("./hangman/_hangman.js"),
+  help:               require("./help/_help.js"),
+  main:               require("./_main.json"),
+  admins:             require("./admins.json"),
+  magic8ball:         require("./magic8ball.json"),
+  messageResponder:   require("./message_responder.json"),
+  mute:               require("./mute.json")
 }
