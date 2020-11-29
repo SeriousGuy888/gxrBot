01c39993e270eb24939312ee91dad055f91eb857
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-11-28 22:14:49 -0500

    g9lbot now displays days remaining in 2020 in status

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,18 @@ module.exports = (client, message) => {
   console.log("g9lBot Loaded!")
 
   client.user.setPresence({ status: "online" })
-  client.user.setActivity(`for ${index.prefix}h`, { type: "WATCHING" })
+  client.user.setActivity(`waiting...`, { type: "WATCHING" })
+
+  setInterval(() => {
+    const currentDate = new Date()
+    const countdownDate = new Date("January 1 2021 00:00:00 GMT-0500") // new years eastern standard time
+
+    const diffMillis = countdownDate - currentDate
+    console.log(diffMillis)
+    index.timeConvert(diffMillis).then(res => {
+      client.user.setActivity(`${res.d} days remaining...`, { type: "WATCHING" })
+    })
+  }, 10 * 1000)
 
   setInterval(() => {
     index.propaganda()
