9124842ce13adb85526186c8b5ff473c0a5fe179
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-11-29 11:56:53 -0500

    rewrote timeConvert to be less garbage

--- functions/timeConvert.js
+++ functions/timeConvert.js
@@ -1,14 +1,21 @@
 exports.run = async (client, milliseconds) => {
+  const unitRatios = {
+    d: 1000 * 60 * 60 * 24,
+    h: 1000 * 60 * 60,
+    m: 1000 * 60,
+    s: 1000
+  }
+
   let ms = milliseconds
   
-  let d = Math.floor(ms / 1000 / 60 / 60 / 24)
-  ms -= d * 1000 * 60 * 60 * 24
-  let h = Math.floor(ms / 1000 / 60 / 60)
-  ms -= h * 1000 * 60 * 60
-  let m = Math.floor(ms / 1000 / 60)
-  ms -= m * 1000 * 60
-  let s = Math.floor(ms / 1000)
-  ms -= s * 1000
+  let d = Math.floor(ms / unitRatios.d)
+  ms -= d * unitRatios.d
+  let h = Math.floor(ms / unitRatios.h)
+  ms -= h * unitRatios.h
+  let m = Math.floor(ms / unitRatios.m)
+  ms -= m * unitRatios.m
+  let s = Math.floor(ms / unitRatios.s)
+  ms -= s * unitRatios.s
 
   return {
     d,
