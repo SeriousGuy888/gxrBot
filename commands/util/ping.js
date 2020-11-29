2e92658a31772a421106fc6a7402947e31541b19
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-11-29 18:48:06 -0500

    added g9lbot's birthday to ping command

--- commands/util/ping.js
+++ commands/util/ping.js
@@ -3,11 +3,15 @@ exports.run = async (client, message, args) => {
   const config = index.config
   const prefix = index.prefix
   const Discord = index.Discord
+  const timeConvert = index.timeConvert
 
-  const uptime = await index.timeConvert(client.uptime)
+  const uptime = await timeConvert(client.uptime)
   let uptimeStr = ""
   for(let i in uptime)
     uptimeStr += `${uptime[i]}${i}`
+  
+  const age = await timeConvert(new Date() - client.user.createdAt)
+  let ageYears = (age.d / 365).toFixed(2)
 
   // gets the name of the command used
   const cmdName = message.content.slice(prefix.length).trim().split(/ +/g).shift().toLowerCase().trim().slice(0, config.main.maxCommandNameLength)
@@ -20,6 +24,7 @@ exports.run = async (client, message, args) => {
       `:arrows_clockwise: Latency: \`${Math.round(client.ws.ping)} ms\``,
       `:clock530: Uptime: \`${uptimeStr}\``
     ].join("\n"))
+    .addField(":cake: Age", `I'm ${ageYears} ${ageYears == 1 ? "year" : "years"} old!\n(I began my existence on ${client.user.createdAt.toUTCString()})`)
     
   message.channel.send(pingEmb)
 }
