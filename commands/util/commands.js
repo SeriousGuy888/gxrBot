30527cec0fb20e9aaa11f3c77ffb3f7ec2d7046e
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-11-29 18:08:52 -0500

    added command `commands` for list of public commands

---
+++ commands/util/commands.js
@@ -0,0 +1,25 @@
+exports.run = async (client, message, args) => {
+  const index = require("../../index.js")
+  
+  let commandListMessage = [
+    "**Public Command List**",
+    "`This list does not include disabled commands or dev commands or their aliases.`",
+    "`cmd_a → cmd_b means that cmd_a is an alias of cmd_b`",
+    "\n"
+  ].join("\n")
+  for(let loopCommandName of client.publicCommandList) {
+    let isAlias = false
+    let redirCmdName
+    if(client.commands.get(loopCommandName).alias) {
+      isAlias = true
+      redirCmdName = client.commands.get(loopCommandName).alias
+      if(client.commands.get(redirCmdName).disabled || client.commands.get(redirCmdName).dev)
+        continue
+    }
+    commandListMessage += `\n• ${loopCommandName}${isAlias ? " → `" + redirCmdName + "`" : ""}`
+  }
+  commandListMessage += "\n\n:warning: *This list is automatically compiled. For a list list with detailed info and that is actively maintained, use command `-help cmds`*"
+
+  for(let loopChunk of commandListMessage.match(/.{1,2000}/gs))
+    message.channel.send(loopChunk)
+}
