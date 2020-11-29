8b00471bb3b02ada2fc896a2fc4ed0eed2b19a3e
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-11-29 16:45:38 -0500

    new year countdown command now has new year case

--- commands/fun/new_year_countdown.js
+++ commands/fun/new_year_countdown.js
@@ -11,17 +11,28 @@ exports.fireWithoutUser = async (client, channel) => {
   const countdownDate = new Date("January 1 2021 00:00:00 GMT-0500") // new years eastern standard time
   const diffMillis = countdownDate - currentDate
 
-  const countdown = await timeConvert(diffMillis)
-  
-  const emb = new Discord.MessageEmbed()
-    .setColor("#12d812")
-    .setTitle(":tada: Countdown to 2021")
-    .setDescription("00:00:00 EST on Monday, January 1st, 2021")
-    .addField("Days", countdown.d, true)
-    .addField("Hours", countdown.h, true)
-    .addField("Minutes", countdown.m, true)
-    .addField("Seconds", countdown.s, true)
-    .addField("Milliseconds", countdown.ms, true)
-    .setFooter("Can we get an uncursed year this time?")
+  let emb = new Discord.MessageEmbed()
+
+  if(diffMillis > 0) {
+    const countdown = await timeConvert(diffMillis)
+    emb
+      .setColor("#12d812")
+      .setTitle(":hourglass: Countdown to 2021")
+      .setDescription("00:00:00 EST on Monday, January 1st, 2021")
+      .addField("Days", countdown.d, true)
+      .addField("Hours", countdown.h, true)
+      .addField("Minutes", countdown.m, true)
+      .addField("Seconds", countdown.s, true)
+      .addField("Milliseconds", countdown.ms, true)
+      .setFooter("Can we get an uncursed year this time?")
+  }
+  else {
+    emb
+      .setColor("#df9e00")
+      .setTitle(":tada: Happy New Year")
+      .setDescription("2020 has finally ended :D")
+      .setFooter("Could this year be uncursed please?")
+  }
+
   channel.send(emb)
 }
