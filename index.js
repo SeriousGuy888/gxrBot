1fb2c69e9510a6762ae6bd98e6d36436cb03e911
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-12-01 20:30:17 -0500

    client partials now listen for everything

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ require("dotenv").config() // .env
 
 const fs = require("fs")
 const Discord = require("discord.js")
-const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] }) // discord client with partials
+const client = new Discord.Client({
+  partials: [ // discord client with partials
+    "USER",
+    "CHANNEL",
+    "MESSAGE",
+    "GUILD_MEMBER",
+    "REACTION"
+  ]
+})
 const config = require("./config/_config.js") // config file
 const prefix = config.main.prefix // bot prefix
 
