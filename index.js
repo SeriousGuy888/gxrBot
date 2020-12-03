2a08cc1644ca127242c235e9c65b72c7dff903bd
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-12-03 10:27:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -159,7 +159,7 @@ module.exports = {
 // * when a sigterm signal is received,
 // * write pending karma to database before
 // * shutting down
-process.once("SIGTERM", async () => {
+process.once("beforeExit", async () => {
   await updateKarma()
   client.util.get("logger").uploadLogs()
     .then(() => {
