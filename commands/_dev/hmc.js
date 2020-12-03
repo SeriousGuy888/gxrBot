d94cfa8208052dd04ec3c7533e25b7726ddfe1cf
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-12-03 10:49:57 -0500

    moved hangman import in hmc

--- commands/_dev/hmc.js
+++ commands/_dev/hmc.js
@@ -1,8 +1,7 @@
-const { hangman } = require("../../config/_config.js")
-
 exports.run = async (client, message, args) => {
   const index = require("../../index.js")
   const config = index.config
+  const { hangman } = config
   const hangmanCache = index.gameCache.hangman
 
   if(message.author.id !== config.admins.superadmin.id)
