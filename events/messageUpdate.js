d3d46b2150a32aec2b16a66ff5743669719d9a12
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-12-04 21:30:56 -0500

    message edits now use coopChannels punish function

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,24 +1,30 @@
 module.exports = (client, oldMessage, newMessage) => {
-  const { config } = require("../index.js")
+  const index = require("../index.js")
+  const { config, coopChannels } = index
 
   if(config.coopchannels.cult.channel == oldMessage.channel.id) {
-    if(newMessage.content.toLowerCase() == config.coopchannels.cult.phrase || oldMessage.author.id == client.user.id) return
-    newMessage.delete().then(() => {
-      if(newMessage.author.bot) return
-      
-      newMessage.channel.send(`<@${newMessage.author.id}>, editing your message to say \`${newMessage.content}\` is a violation of the cult rules.\nYou may only say \`${config.coopchannels.cult.phrase}\` here.`)
-        .then(msg => msg.delete(3000))
-        .catch(console.error)
-    })
+    if(newMessage.content.toLowerCase() == config.coopchannels.cult.phrase || oldMessage.author.id == client.user.id)
+      return
+    newMessage.delete()
+      .then(() => {
+        if(newMessage.author.bot)
+          return
+        
+        coopChannels.punish(newMessage, "cult", [
+          config.coopchannels.cult.phrase,
+          newMessage.content
+        ])
+      })
   }
   else if(config.coopchannels.ows.channel == oldMessage.channel.id) {
-    if(newMessage.content.split(" ").length == 1 || newMessage.author.id == client.user.id) return
-    newMessage.delete().then(() => {
-      if(newMessage.author.bot) return
-      
-      newMessage.channel.send(`<@${message.author.id}>, this is the one word story channel. You are a stupid.`)
-        .then(msg => msg.delete(3000))
-        .catch(console.error)
-    })
+    if(newMessage.content.split(" ").length == 1 || newMessage.author.id == client.user.id)
+      return
+    newMessage.delete()
+      .then(() => {
+        if(newMessage.author.bot)
+          return
+        
+        coopChannels.punish(newMessage, "ows")
+      })
   }
 }
