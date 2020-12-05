a66b6e7670f32a35f023b2c963f48813ecc84e08
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-12-05 13:13:37 -0500

    removed propaganda function imports

--- index.js
+++ index.js
@@ -136,7 +136,6 @@ const autoResponses = client.functions.get("autoResponses").run
 const commandHelpEmbed = client.functions.get("commandHelpEmbed").run
 const coopChannels = client.functions.get("coopChannels").run
 const messageResponder = client.functions.get("messageResponder").run
-// const propaganda = client.functions.get("propaganda").run
 const timeConvert = client.functions.get("timeConvert").run
 const updateKarma = client.functions.get("updateKarma").run
 const voteReactions = client.functions.get("voteReactions").run
@@ -167,7 +166,6 @@ module.exports = {
   commandHelpEmbed,
   coopChannels,
   messageResponder,
-  // propaganda,
   timeConvert,
   updateKarma,
   voteReactions,
