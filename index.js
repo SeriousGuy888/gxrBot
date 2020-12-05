46eb5ecd184df75d2afc20c2611452ec3891de36
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-12-05 13:09:50 -0500

    disabled propaganda function

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ const autoResponses = client.functions.get("autoResponses").run
 const commandHelpEmbed = client.functions.get("commandHelpEmbed").run
 const coopChannels = client.functions.get("coopChannels").run
 const messageResponder = client.functions.get("messageResponder").run
-const propaganda = client.functions.get("propaganda").run
+// const propaganda = client.functions.get("propaganda").run
 const timeConvert = client.functions.get("timeConvert").run
 const updateKarma = client.functions.get("updateKarma").run
 const voteReactions = client.functions.get("voteReactions").run
@@ -167,7 +167,7 @@ module.exports = {
   commandHelpEmbed,
   coopChannels,
   messageResponder,
-  propaganda,
+  // propaganda,
   timeConvert,
   updateKarma,
   voteReactions,
