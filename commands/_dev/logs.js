90e16360d303e8124c4e63824ed9c91cff3368a1
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-12-06 22:19:31 -0500

    logs command passes in log source

--- commands/_dev/logs.js
+++ commands/_dev/logs.js
@@ -6,7 +6,7 @@ exports.run = async (client, message, args) => {
   if(message.author.id !== config.admins.superadmin.id)
     return message.channel.send("You may not use this dev command!")
   
-  logger.log(`${message.author.id} requested log upload`)
+  logger.log(`${message.author.id} requested log upload`, "logs command")
 
   logger.uploadLogs(`Log upload request from ${message.author.tag}`)
     .then(() => message.channel.send(`Logs have been uploaded to <#${config.logger.uploads.channel}>.`))
