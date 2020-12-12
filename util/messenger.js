1589a0b2b2eacc1f26640e72b09426a5d7aad325
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-12-12 16:48:27 -0500

    messenger only sanitizes strings now

--- util/messenger.js
+++ util/messenger.js
@@ -7,6 +7,9 @@ const index = require("../index.js")
 const { client } = index
 
 exports.sanitize = async content => {
+  if(typeof content !== "string")
+    return content
+
   let sanitizedContent = content.match(/(.|\n){1,2000}/g)[0]
   if(sanitizedContent.length === 0)
     sanitizedContent = "**Error:** Message Empty"
