63721eda149dee235945847f934f870640b181e2
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-12-11 22:11:11 -0500

    web panel links for individual users added

--- commands/karma/leaderboard.js
+++ commands/karma/leaderboard.js
@@ -90,7 +90,7 @@ exports.run = async (client, message, args) => {
 
     let karma = field.karma
     let emoji = karma > 0 ? positive : negative
-    let content = `${emoji} [${karma.toLocaleString()}](${settings.lang.web_panel_lookup_link}?ids=${field.id})`
+    let content = `${emoji} [${karma.toLocaleString()}](${settings.lang.web_panel_lookup_link}?ids=${field.id} "See this user on the web panel.")`
 
     if(karmaQueue[field.id])
       content += ` and ${karmaQueue[field.id]} pending`
