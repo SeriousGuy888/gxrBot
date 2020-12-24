f55c526c0556816cd7e7ad49f01636d7cd526e06
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-12-23 23:08:05 -0500

    redid ping command

--- commands/util/ping.js
+++ commands/util/ping.js
@@ -8,19 +8,32 @@ exports.run = async (client, message, args) => {
   // gets the name of the command used
   const cmdName = message.content.slice(prefix.length).trim().split(/ +/g).shift().toLowerCase().trim().slice(0, config.main.maxCommandNameLength)
 
+  const loadingEmoji = config.main.emojis.loading
 
   const uptimeStr = await timer.stringify(client.uptime, { truncZero: true })
   let pingEmb = new Discord.MessageEmbed()
     .setColor(config.main.colours.success)
     .setTitle(cmdName.toLowerCase() == "pong" ? "Ping!" : "Pong!") // will say ping if the command alias pong was used
+    .setDescription(loadingEmoji)
     .addField(":clock530: Uptime", `\`${uptimeStr}\``, true)
-    .addField(":arrows_clockwise: Latency", `\`${Math.round(client.ws.ping)} ms\``, true)
-    .addField(":cake: Age", `I'm \`${ageYears}\` ${ageYears == 1 ? "year" : "years"} old!\n(${client.user.createdAt.toUTCString()})`)
-
+    .addField(":cake: Age", `I'm \`${ageYears}\` ${ageYears == 1 ? "year" : "years"} old!\n(Dec 18, 2019)`, true)
   if(process.env.DEV_MODE)
     pingEmb.addField(":keyboard: Dev Mode", `\`${process.env.DEV_MODE}\``)
     
-  message.channel.send(pingEmb)
+
+  const msg = await message.channel.send(pingEmb)
+
+  const getRoundTripLatency = () => msg.createdTimestamp - message.createdTimestamp
+  const getEmbedDescription = () => {
+    return [
+      "*`Any of these being negative is probably bad.`*",
+      `DiscordAPI Shard Latency: \`${client.ws.ping}ms\``,
+      `Message Round Trip Latency: \`${getRoundTripLatency()}ms\``,
+    ].join("\n")
+  }
+
+  pingEmb.setDescription(getEmbedDescription())
+  msg.edit(pingEmb)
 }
 
 exports.help = async (client, message, args) => {
