ae9295c3c926dc0c68362729456044e7bf12f87f
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-12-28 16:07:59 -0500

    web panel link command

---
+++ commands/util/web.js
@@ -0,0 +1,11 @@
+exports.run = async (client, message, args) => {
+  const index = require("../../index.js")
+  const { config, Discord } = index
+
+  let emb = new Discord.MessageEmbed()
+    .setColor(config.main.colours.success)
+    .setTitle("Web Panel Link")
+    .setDescription(`[Here](${config.main.links.web_panel})`)
+  
+  message.channel.send(emb)
+}
