8763a2bd7daac83d2656f4af964ad3997fa5cbc8
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-01-03 17:01:43 -0500

    paginating inventory command

--- commands/economy/inventory.js
+++ commands/economy/inventory.js
@@ -3,13 +3,25 @@ exports.run = async (client, message, args) => {
   const { Discord, config, banker, messenger } = index
   const settings = config.economy
   
-  let member
+
+  let page = 1
+  if(args[1])
+    page = parseInt(args[1])
+  if(args[0])
+    page = parseInt(args[0])
+
+
+  let member 
   if(!args[0])
     member = message.author
   else
-    member = message.mentions.members.first() || await message.guild.members.fetch(args[0])
+    member = message.mentions.members.first() || await message.guild.members.fetch(args[0]).catch(() => {})
+  
+  if(!member)
+    member = message.author
   if(member.user)
     member = member.user
+  
   
   
   const msg = await messenger.loadingMessage(message.channel, {
@@ -18,14 +30,35 @@ exports.run = async (client, message, args) => {
   })
 
 
+  const inventory = await banker.getInventory(member.id)
+  const inventoryKeys = Object.keys(inventory).sort()
+  const sortedInventory = {}
+
+  const itemsPerPage = 5
+  const pageCount = Math.ceil(inventoryKeys.length / itemsPerPage)
+  let itemsLooped = 0
+  let itemNumber = itemsPerPage * (page - 1)
+  let itemsAdded = 0
+
+  for(const loopKey of inventoryKeys)
+    sortedInventory[loopKey] = inventory[loopKey]
+
   const responseEmbed = new Discord.MessageEmbed()
     .setColor(settings.colours.generic)
     .setTitle(`Inventory of ${member.tag}`)
+    .setFooter(`Page ${page} of ${pageCount}`)
+  
 
-  const inventory = await banker.getInventory(member.id)
-  if(Object.keys(inventory).length) {
-    for(const item in inventory) {
-      responseEmbed.addField(item, inventory[item])
+  if(inventoryKeys.length) {
+    for(const item in sortedInventory) {
+      itemsLooped++
+      if(itemsLooped < itemNumber + 1)
+        continue
+      if(itemsAdded >= itemsPerPage)
+        break
+      responseEmbed.addField(item, sortedInventory[item])
+      itemNumber++
+      itemsAdded++
     }
   }
   else {
