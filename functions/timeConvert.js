b78b47d65dc0a7b7a6930ef7c0d8fd3fea59719d
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-01-03 18:08:06 -0500

    delete legacy timeConvert function

--- functions/timeConvert.js
+++
@@ -1,7 +0,0 @@
-exports.run = async (milliseconds) => {
-  const index = require("../index.js")
-  const { timer } = index
-
-  console.trace(`Use of legacy timeConvert function. Should be converted to timer util.`)
-  return timer.convert(milliseconds)
-}
