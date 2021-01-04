723cb5e4980d7306a6fa2e24b98abdac575ce0a2
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-01-03 19:48:56 -0500

    embedder.addAuthor function

--- util/embedder.js
+++ util/embedder.js
@@ -3,3 +3,9 @@ const Discord = index.Discord
 
 exports.newEmbed = () => new Discord.MessageEmbed()
 exports.addBlankField = (embed, inline) => embed.addField("\u200b", "\u200b", !!inline)
+exports.addAuthor = (embed, user, authorText) => {
+  return embed.setAuthor(
+    authorText.replace(/%tag%/gi, user.tag) ?? user.tag,
+    user.avatarURL({ dynamic: true })
+  )
+}
