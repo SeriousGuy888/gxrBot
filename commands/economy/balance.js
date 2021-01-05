83b28b0357af5ee1c9395e8b8d9482593c972e6e
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-01-05 14:49:24 -0500

    add info for earning moolah in balance command

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -13,6 +13,7 @@ exports.run = async (client, message, args) => {
 
   const responseEmbed = new Discord.MessageEmbed()
     .setColor(settings.colours.generic)
+    .setFooter("You can earn moolah by participating in the one word story.")
   embedder.addAuthor(responseEmbed, user, "%tag%'s Balance")
 
   const balance = await banker.getBalance(user.id)
