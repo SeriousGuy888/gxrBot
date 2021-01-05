d8fe8a4089b38671d903b90704f7af03cb8fa949
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-01-05 13:12:00 -0500

    added `use` command to use inv items

---
+++ commands/economy/use.js
@@ -0,0 +1,35 @@
+exports.run = async (client, message, args) => {
+  const index = require("../../index.js")
+  const { config, banker, embedder, messenger } = index
+  const settings = config.economy.settings
+  const itemConfig = config.economy.items
+  
+
+  if(!args[0]) {
+    message.channel.send("specify an item id")
+    return
+  }
+  const item = args[0]
+  
+  const msg = await messenger.loadingMessage(message.channel, { colour: settings.colours.generic })
+
+
+  const inventory = await banker.getInventory(message.author.id)
+  if(!inventory[item])
+    return msg.edit("you do not have this item")
+  if(!itemConfig[item])
+    return msg.edit("unknown item specified")
+  if(!itemConfig[item].use)
+    return msg.edit("item cannot be used")
+    
+  const res = await itemConfig[item].use(message.author.id)
+  const emb = embedder.newEmbed()
+    .setColor(settings.colours.generic)
+    .setTitle(`Used Item \`${item}\``)
+    .setDescription(res ?? "No more information was provided.")
+  embedder.addAuthor(emb, message.author)
+
+  msg.edit(emb)
+}
+
+exports.cooldown = 5
