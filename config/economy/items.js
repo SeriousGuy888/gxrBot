670fc77020b353999f6811fe3ab84132cc9f37d3
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-01-05 13:11:51 -0500

    inventory items now have `use` functions

--- config/economy/items.js
+++ config/economy/items.js
@@ -1,6 +1,15 @@
 module.exports = {
   test: {
     emoji: ":duck:",
-    description: "A developer item."
+    description: "A developer item.",
+    use: async userId => {
+      const index = require("../../index.js")
+      const { banker } = index
+
+      banker.addToBalance(userId, 50)
+      banker.addToInventory(userId, "test", -1)
+
+      return "Added 50 to your balance."
+    }
   }
 }
