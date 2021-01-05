7bfeed6bc05b929256bdd2d23972d44a37683af2
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-01-05 15:07:19 -0500

    removed new year countdown from status

--- events/ready.js
+++ events/ready.js
@@ -1,28 +1,13 @@
 module.exports = async (client, message) => {
   const index = require("../index.js")
-  const { awaitOrders, config, schedule, updateKarma, banker, logger, timer } = index
+  const { awaitOrders, config, schedule, updateKarma, banker, logger } = index
 
   awaitOrders()
   logger.log(`${config.main.botNames.lowerCamelCase} successfully loaded ${process.env.DEV_MODE ? "in dev mode" : ""}`)
 
   client.user.setPresence({ status: "online" })
-  client.user.setActivity(`waiting for data...`, { type: "WATCHING" })
-
-  setInterval(async () => {
-    const currentDate = new Date()
-    const countdownDate = new Date("January 1 2021 00:00:00 GMT-0500") // new years eastern standard time
-    const diffMillis = countdownDate - currentDate
-    const timeRemaining = await timer.convert(diffMillis)
-
-    if(diffMillis > 0) {
-      const pad = num => num.toString().padStart(2, "0")
-      let { h, m, s } = timeRemaining
-      client.user.setActivity(`${h}:${pad(m)}:${pad(s)} remaining...`, { type: "WATCHING" })
-    }
-    else {
-      client.user.setActivity("🎆 Happy new year!", { type: "WATCHING" })
-    }
-  }, 7000)
+  client.user.setActivity(`Added ${prefix}bal command!`, { type: "WATCHING" })
+  
   
   schedule.scheduleJob("0 * * * *", () => {
     client.util.logger.uploadLogs("Hourly automatic log upload")
