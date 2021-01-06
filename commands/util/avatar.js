3126407e5e3424160d7066f31e0bd641e1aa8261
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-01-05 21:53:25 -0500

    add avatar command

---
+++ commands/util/avatar.js
@@ -0,0 +1,14 @@
+exports.run = async (client, message, args) => {
+  const index = require("../../index.js")
+  const { config, Discord, getUserArg, embedder } = index
+
+  const user = await getUserArg(message)
+
+  const emb = new Discord.MessageEmbed()
+    .setColor(config.main.colours.success)
+    .setDescription("Here you go!")
+    .setImage(user.avatarURL({ dynamic: true }))
+  embedder.addAuthor(emb, user)
+
+  message.channel.send(emb)
+}
