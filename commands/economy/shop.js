f34d724969b226bc4e484868b4e42b62f13ca722
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-01-06 13:53:55 -0500

    paghe moving buttons in shop command

--- commands/economy/shop.js
+++ commands/economy/shop.js
@@ -10,44 +10,78 @@ exports.run = async (client, message, args) => {
   const itemKeys = Object.keys(itemConfig).sort()
   const maxPages = Math.ceil(itemKeys.length / itemsPerPage)
 
-  const page = Math.min(Math.abs(args[0]) || 1, maxPages)
+  let page = Math.min(Math.abs(args[0]) || 1, maxPages)
   
-  const responseEmbed = new Discord.MessageEmbed()
-    .setColor(settings.colours.generic)
-    .setTitle("Item Shop")
-    .setDescription("You can use the `buy` and `sell` commands.")
-    .setFooter(`Page ${page} of ${maxPages}`)
-  embedder.addAuthor(responseEmbed, message.author)
+  let msg
+  const displayShop = async () => {
+    page = Math.max(Math.min(page, maxPages), 1)
 
-  for(let i = 0; i < itemsPerPage; i++) {
-    const itemIndex = i + ((page - 1) * itemsPerPage)
-    const itemKey = itemKeys[itemIndex]
+    const responseEmbed = new Discord.MessageEmbed()
+      .setColor(settings.colours.generic)
+      .setTitle("Item Shop")
+      .setDescription("You can use the `buy` and `sell` commands.")
+      .setFooter(`Page ${page} of ${maxPages}`)
+    embedder.addAuthor(responseEmbed, message.author, "%tag%'s Shop Panel")
+  
+    for(let i = 0; i < itemsPerPage; i++) {
+      const itemIndex = i + ((page - 1) * itemsPerPage)
+      const itemKey = itemKeys[itemIndex]
+  
+      if(!itemKey)
+        break
+  
+      const itemInfo = itemConfig[itemKey]
+  
+      const itemEmoji = itemInfo.emoji + " " ?? ""
+      const itemName = itemInfo.name ? itemEmoji + itemInfo.name : itemKey
+      const itemDescription = itemInfo.description ?? "No description provided."
+  
+      const itemValue = itemInfo.value ?? {}
+      const itemBuy = itemValue.buy ? coin + itemValue.buy.toLocaleString() : "Cannot be purchased"
+      const itemSell = itemValue.sell ? coin + itemValue.sell.toLocaleString() : "Cannot be sold"
 
-    if(!itemKey)
-      break
 
-    const itemInfo = itemConfig[itemKey]
+      responseEmbed.addField(
+        itemName,
+        [
+          (itemName ? `** Item ID:** \`${itemKey}\`` : ""),
+          `**Buy Price:** ${itemBuy}`,
+          `**Sell Price:** ${itemSell}`,
+          "",
+          itemDescription
+        ].join("\n"),
+        true
+      )
+    }
+  
 
-    const itemEmoji = itemInfo.emoji + " " ?? ""
-    const itemName = itemInfo.name ? itemEmoji + itemInfo.name : itemInfo.id
-    const itemDescription = itemInfo.description ?? "No description provided."
+    msg = await message.channel.send(responseEmbed)
 
-    const itemValue = itemInfo.value ?? {}
-    const itemBuy = itemValue.buy ? coin + itemValue.buy.toLocaleString() : "Cannot be purchased"
-    const itemSell = itemValue.sell ? coin + itemValue.sell.toLocaleString() : "Cannot be sold"
-
-    responseEmbed.addField(
-      itemName,
-      [
-        (itemName ? `** Item ID:** \`${itemKey}\`` : ""),
-        `**Buy Price:** ${itemBuy}`,
-        `**Sell Price:** ${itemSell}`,
-        "",
-        itemDescription
-      ].join("\n"),
-      true
-    )
+    const reactionEmojis = ["⏪", "⬅️", "➡️", "⏩"]
+    for(const loopEmoji of reactionEmojis)
+      await msg.react(loopEmoji)
+    const reactionFilter = (reaction, user) => user.id == message.author.id && reactionEmojis.includes(reaction.emoji.name)
+    msg.awaitReactions(reactionFilter, { max: 1, time: 15000 })
+      .then(async collected => {
+        const emojiName = collected.first().emoji.name
+        switch(emojiName) {
+          case "⏪":
+            page = 1
+            break
+          case "⬅️":
+            page--
+            break
+          case "➡️":
+            page++
+            break
+          case "⏩":
+            page = maxPages
+            break
+        }
+        msg = await displayShop()
+      })
+      .catch(() => {})
   }
 
-  message.channel.send(responseEmbed)
+  await displayShop()
 }
