51993e0af05c74b15a0aa8b6205e5cb1797fc1e2
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-01-06 10:11:55 -0500

    avatar command gives larger avatar size

--- commands/util/avatar.js
+++ commands/util/avatar.js
@@ -7,7 +7,7 @@ exports.run = async (client, message, args) => {
   const emb = new Discord.MessageEmbed()
     .setColor(config.main.colours.success)
     .setDescription("Here you go!")
-    .setImage(user.avatarURL({ dynamic: true }))
+    .setImage(user.avatarURL({ dynamic: true, size: 2048 }))
   embedder.addAuthor(emb, user, "%tag%'s Avatar")
 
   message.channel.send(emb)
