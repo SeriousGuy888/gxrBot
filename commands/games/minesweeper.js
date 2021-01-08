6295f3e99a68c98b830d7ee38b0bb4349147cca3
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-01-08 13:32:48 -0500

    win detection without flags

--- commands/games/minesweeper.js
+++ commands/games/minesweeper.js
@@ -1,10 +1,13 @@
 exports.run = async (client, message, args) => {
   const index = require("../../index.js")
+  const minesweeperCache = index.gameCache.minesweeper
   const { config, Discord, embedder } = index
 
   const columnCount = 12 // x
   const rowCount = 12 // y
-  const totalMines = 12
+  const totalMines = 24
+
+  const playerData = minesweeperCache[message.author.id]
 
   const create2dArray = (cols, rows) => {
     let arr = new Array(cols)
@@ -75,7 +78,7 @@ exports.run = async (client, message, args) => {
 
   const checkAdjacentMines = (field, x, y) => {
     if(field[x][y].mine)
-      return
+      return -1
 
     let mineCount = 0
 
@@ -103,6 +106,34 @@ exports.run = async (client, message, args) => {
     return minedField
   }
 
+  const revealSquare = async (field, x, y) => {
+    if((!field[x]) || (!field[x][y]))
+      return -1
+    
+    field[x][y].revealed = true
+
+    if(field[x][y].mine)
+      playerData.failure = true
+
+    if(!checkAdjacentMines(field, x, y)) {
+      for(let xOffset = -1; xOffset <= 1; xOffset++) {
+        for(let yOffset = -1; yOffset <= 1; yOffset++) {
+          if(xOffset === 0 && yOffset === 0)
+            continue
+          if(
+            (!field[x + xOffset]) ||
+            (!field[x + xOffset][y + yOffset]) ||
+            field[x + xOffset][y + yOffset].revealed
+          ) continue
+
+          revealSquare(field, x + xOffset, y + yOffset)
+        }
+      }
+    }
+
+    return field
+  }
+
   const renderField = (field) => {
     let fieldRows = []
 
@@ -119,7 +150,7 @@ exports.run = async (client, message, args) => {
           else {
             const mineCount = checkAdjacentMines(field, x, y)
             if(mineCount === 0) {
-              fieldRows[x].push("⬛")
+              fieldRows[x].push(((x + 1) % 5 === 0 || (y + 1) % 5 === 0) ? "◾" : "⬛") 
             }
             else {
               fieldRows[x].push(getNumberEmoji(mineCount))
@@ -131,13 +162,7 @@ exports.run = async (client, message, args) => {
             fieldRows[x].push("🚩")
           }
           else { // if the square is not flagged
-            let orangify = false // make a checkerboard pattern
-            if(x % 2 === 0)
-              orangify = !orangify
-            if(y % 2 === 0)
-              orangify = !orangify
-            
-            fieldRows[x].push(orangify ? "🟧" : "🟨")
+            fieldRows[x].push(((x + 1) % 5 === 0 || (y + 1) % 5 === 0) ? "🟧" : "🟨")
           }
         }
       }
@@ -150,15 +175,135 @@ exports.run = async (client, message, args) => {
     return fieldRows.join("\n")
   }
 
-  const gameField = createField(columnCount, rowCount)
+  const getFlagCount = (field) => {
+    let flagCount = 0
 
-  const emb = new Discord.MessageEmbed()
-    .setColor(config.main.colours.success)
-    .setTitle("Minesweeper")
-    .setDescription(renderField(gameField))
-    .addField("Mines", totalMines, true)
-    .addField("Field Size", `${columnCount}x${rowCount}`, true)
-  embedder.addAuthor(emb, message.author)
+    for(let x = 0; x < field.length; x++) {
+      for(let y = 0; y < field[x].length; y++) {
+        if(field[x][y].flagged)
+          flagCount++
+      }
+    }
+    
+    return flagCount
+  }
 
-  message.channel.send(emb)
+  const revealAllMines = async (field, flag) => {
+    for(let x = 0; x < field.length; x++) {
+      for(let y = 0; y < field[x].length; y++) {
+        if(field[x][y].mine) {
+          if(flag)
+            field[x][y].flagged = true
+          else
+            field[x][y].revealed = true
+        }
+      }
+    }
+  }
+
+  const checkGameOver = async (field) => {
+    if(getFlagCount(field) > totalMines)
+      return null
+    
+    if(playerData && playerData.failure)
+      return { win: false }
+
+    
+    for(let x = 0; x < field.length; x++) {
+      for(let y = 0; y < field[x].length; y++) {
+        if(field[x][y].revealed) { // square is revealed
+          if(field[x][y].mine) { // square is mine
+            // await revealAllMines(field)
+            // console.log("mine")
+            return { win: false }
+          }
+        }
+        else { // square is hidden
+          if(!field[x][y].mine) { // square is safe
+            // console.log("safe")
+            return null // game is not finished
+          }
+        }
+      }
+    }
+
+    return { win: true }
+  }
+
+
+
+  const tickMinesweeper = async (channel, options, init) => {
+    let gameField, msg
+
+    if(init)
+      gameField = createField(columnCount, rowCount)
+    else
+      gameField = playerData.field
+
+    if(options) {
+      if(options.reveal) {
+        const squareCoords = options.reveal
+        await revealSquare(gameField, squareCoords[0], squareCoords[1])
+        console.log(gameField[squareCoords[0]][squareCoords[1]])
+      }
+    }
+
+
+    const gameOver = await checkGameOver(gameField)
+    if(gameOver) {
+      await revealAllMines(gameField, gameOver.win)
+    }
+
+    const emb = new Discord.MessageEmbed()
+      .setColor(config.main.colours.success)
+      .setTitle("Minesweeper but bad")
+      .setDescription(renderField(gameField))
+      .addField("Field Size", `${columnCount}x${rowCount}`, true)
+      .addField("Flags Placed", `${getFlagCount(gameField)} / ${totalMines}`, true)
+    embedder.addAuthor(emb, message.author)
+
+    if(!gameOver) {
+      if(init) {
+        minesweeperCache[message.author.id] = {
+          failure: false,
+          message: await channel.send(emb),
+          field: gameField,
+        }
+      }
+      else {
+        msg = playerData.message
+        msg.edit(emb)
+      }
+    }
+    else {
+      msg = playerData.message
+
+      emb.setColor(gameOver.win ? config.main.colours.success : config.main.colours.error)
+      msg.edit(gameOver.win ? "You win!" : "You got blown up by a landmine D:", { embed: emb })
+
+      delete minesweeperCache[message.author.id]
+    }
+  }
+
+  if(!args[0])
+    tickMinesweeper(message.channel, null, true)
+  else {
+    if((!playerData) || (!playerData.message)) {
+      message.channel.send("you dont have an ongoing minesweeper game you dukc")
+      return
+    }
+
+    let coords = args[0]
+      .split(",") // quakc
+      .splice(0, 2) // get first two coordinates
+      .reverse() // coords are reversed and i dont know why but this fixes it probably
+    coords = coords.map(elem => parseInt(elem) - 1)
+
+    if(coords.length < 2) {
+      message.channel.send("not enough dukcing coordinates you dukcing dukc")
+      return
+    }
+    
+    tickMinesweeper(message.channel, { reveal: coords })
+  }
 }
