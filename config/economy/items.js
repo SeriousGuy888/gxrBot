53940a0562473edfe952bb0f94e1416bfb184d21
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-01-27 12:52:57 -0500

    updated 2021 medal descro[topm

--- config/economy/items.js
+++ config/economy/items.js
@@ -24,6 +24,6 @@ module.exports = {
   twenty_twenty_one: {
     emoji: "<:2021:796514745837682688>",
     name: "2021 New Year Medal",
-    description: "Was awarded to all users who were members of the discord server at the start of 2021."
+    description: "Was awarded to all users who were members of the Grade 9 League server at the start of 2021."
   }
 }
