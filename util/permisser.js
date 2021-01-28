ca368435f7332b7382527fafa11565ea8b08d221
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-01-28 10:50:26 -0500

    permisser.hasPermission now takes arrays

--- util/permisser.js
+++ util/permisser.js
@@ -2,12 +2,25 @@ const index = require("../index.js")
 const { Discord } = index
 
 exports.hasPermission = (guildMember, permissionFlag) => {
+exports.hasPermission = (guildMember, permissionFlags, all) => {
   if(!(guildMember instanceof Discord.GuildMember))
     throw "Invalid GuildMember provided!"
   
-  const permission = Discord.Permissions.FLAGS[permissionFlag.toUpperCase()]
-  if(!permissionFlag)
-    throw "Invalid permission name provided!"
-  
-  return guildMember.hasPermission(permission)
+  let permissionArr
+  if(permissionFlags instanceof Array) // allow arrays
+    permissionArr = permissionFlags
+  else
+    permissionArr = [permissionFlags] // and strings
+
+  let permissionsAllowed = []
+  for(const loopPermissionFlag of permissionArr) {
+    const permission = Discord.Permissions.FLAGS[loopPermissionFlag.toUpperCase()]
+    permissionsAllowed.push(guildMember.hasPermission(permission))
+  }
+
+  if(all)
+    return permissionsAllowed.every(perm => perm) // every single one is true
+  else
+    return permissionsAllowed.some(perm => perm) // at least one is true
+}
 }
