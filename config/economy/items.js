13c866f67cb43bfc0b8c48e212ddb4dccb5830ab
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-01-29 12:39:55 -0500

    Update items.js

--- config/economy/items.js
+++ config/economy/items.js
@@ -20,5 +20,23 @@ module.exports = {
     emoji: "<:2021:796514745837682688>",
     name: "2021 New Year Medal",
     description: "Was awarded to all users who were members of the Grade 9 League server at the start of 2021."
-  }
+  },
+  us_embassy_in_baghdad: {
+    emoji: ":flag_us::homes::flag_iq: ",
+    name: "American Embassy in the City of Baghdad",
+    description: "A very useful item",
+    givable: true,
+    value: {
+      buy: 250000000,
+      sell: -1
+    },
+    use: async userId => {
+      // const index = require("../../index.js")
+      // const { banker } = index
+
+      // banker.addToInventory(userId, "test", -1)
+
+      return "Did nothing."
+    }
+  },
 }
