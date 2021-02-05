d224758e2f548fbb044ae511e16e7d475d7d6220
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-02-05 13:52:43 -0500

    two space indent with everythinbg else

--- commands/games/blackjack.js
+++ commands/games/blackjack.js
@@ -1,26 +1,24 @@
 exports.run = async (client, message, args) => {
-    const index = require("../../index.js")
-    const config = index.config
-    const Discord = index.Discord
+  const index = require("../../index.js")
+  const config = index.config
+  const Discord = index.Discord
   
-    function cardValue(number){
-        if(!number[0].isNan()) return parseInt(number[0])
-        else if(number[0] == "A") return 1
-        else return 10
-    }
-
-    const suits = ["♠️", "♥️", "♣️", "♦️"]
-    const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-    const cards = []
-
-    for(const loopSuit of suits){
-        for(const loopNumber of numbers){
-            cards.push(loopNumber + loopSuit)
-        }
-    }
-
-    console.log(cards)
-
-    
-
+  function cardValue(number){
+    if(!number[0].isNan()) return parseInt(number[0])
+    else if(number[0] == "A") return 1
+    else return 10
   }
+
+  const suits = ["♠️", "♥️", "♣️", "♦️"]
+  const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  const cards = []
+
+  for(const loopSuit of suits){
+    for(const loopNumber of numbers){
+      cards.push(loopNumber + loopSuit)
+    }
+  }
+
+  console.log(cards)
+
+}
