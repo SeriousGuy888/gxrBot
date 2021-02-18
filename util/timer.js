44538e6e755777b4cf90b0a0d286133243edde36
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-02-18 12:53:20 -0500

    add util to parse time strings

--- util/timer.js
+++ util/timer.js
@@ -1,16 +1,18 @@
 // const index = require("../index.js")
 
-exports.convert = async milliseconds => {
-  const unitRatios = {
-    d: 1000 * 60 * 60 * 24,
-    h: 1000 * 60 * 60,
-    m: 1000 * 60,
-    s: 1000
-  }
+const unitRatios = {
+  y: 1000 * 60 * 60 * 24 * 365,
+  w: 1000 * 60 * 60 * 24 * 7,
+  d: 1000 * 60 * 60 * 24,
+  h: 1000 * 60 * 60,
+  m: 1000 * 60,
+  s: 1000
+}
 
+exports.convert = async milliseconds => {
   let ms = milliseconds
   let d, h, m, s = 0
-  
+
   d = Math.floor(ms / unitRatios.d)
   ms -= d * unitRatios.d
 
@@ -30,6 +32,27 @@ exports.convert = async milliseconds => {
     s,
     ms
   }
+}
+
+exports.parse = async (str, min, max) => {
+  if(!min) min = -Infinity
+  if(!max) max = Infinity
+
+  const timeString = str.toLowerCase()
+  const pattern = /(\d+)(y|w|d|h|m|s)/g
+
+  let timespan = 0
+  let match
+
+  while(match = pattern.exec(timeString)) {
+    const magnitude = parseInt(match[1]) // how many hours
+    const unit = match[2] // hours
+    const ratio = unitRatios[unit]
+      
+    timespan += (magnitude * ratio)
+  }
+
+  return Math.max(Math.min((timespan / unitRatios.s), max), min) // return timespan in seconds
 }
 
 exports.stringify = async (milliseconds, options) => {
