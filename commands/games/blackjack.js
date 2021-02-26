b6719e2dd3d385e2ffdc6650fb7bdd5bbe6e5c80
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-02-25 21:56:38 -0500

    blackjack card deck generation

--- commands/games/blackjack.js
+++ commands/games/blackjack.js
@@ -2,22 +2,26 @@ exports.run = async (client, message, args) => {
   const index = require("../../index.js")
   const { config, Discord } = index // import from index.js
   
-  function cardValue(number){
-    if(!number[0].isNan()) return parseInt(number[0])
-    else if(number[0] == "A") return 1
-    else return 10
+  const getCardValue = (number) => {
+    const parsedInt = parseInt(number[0])
+    if(parsedInt)
+      return parsedInt
+    
+    if(number[0] === "A")
+      return 1
+    else
+      return 10
   }
 
   const suits = ["♤", "♡", "♧", "♢"]
   const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-  const cards = []
+  let cards = []
 
-  for(const loopSuit of suits){
-    for(const loopNumber of numbers){
+  for(const loopSuit of suits) {
+    for(const loopNumber of numbers) {
       cards.push(loopNumber + loopSuit)
     }
   }
 
-  console.log(cards)
-
+  message.channel.send(cards.join(","))
 }
