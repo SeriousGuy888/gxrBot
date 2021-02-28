62da14057f62e4646565b54638aada894d59b597
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-02-28 11:39:25 -0500

    removed legacy addKarma function

--- functions/addKarma.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (userId, amount, options) => {
-  const index = require("../index.js")
-  const { client } = index
-  const { karmanator } = client.util
-
-  
-  karmanator.add(userId, amount, options)
-}
