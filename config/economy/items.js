f9c3db871f659454714f08b3cbba3ddde5938e84
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-03-03 16:50:42 -0500

    added way to convert 2021 medal item to badge

--- config/economy/items.js
+++ config/economy/items.js
@@ -19,7 +19,16 @@ module.exports = {
   twenty_twenty_one: {
     emoji: "<:2021:796514745837682688>",
     name: "2021 New Year Medal",
-    description: "Was awarded to all users who were members of the Grade 9 League server at the start of 2021."
+    description: "Was awarded to all users who were members of the Grade 9 League server at the start of 2021.",
+    use: async userId => {
+      const index = require("../../index.js")
+      const { banker, badger } = index.client.util
+
+      banker.addToInventory(userId, "twenty_twenty_one", -1)
+      badger.awardBadge(userId, "2021", false, "conversion of old 2021 award to badge")
+
+      return "Converted your 2021 medal item to a badge."
+    }
   },
   us_embassy_in_baghdad: {
     emoji: ":flag_us::homes::flag_iq: ",
