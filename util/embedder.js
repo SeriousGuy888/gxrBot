0ffb91899d5510bbccc84d87b7d91a81152c0a59
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-03-07 13:06:41 -0500

    fixed possible infinite loop in embedder.addAuthor

--- util/embedder.js
+++ util/embedder.js
@@ -5,7 +5,7 @@ exports.newEmbed = () => new Discord.MessageEmbed()
 exports.addBlankField = (embed, inline) => embed.addField("\u200b", "\u200b", !!inline)
 exports.addAuthor = (embed, user, authorText) => {
   return embed.setAuthor(
-    (authorText ?? user.tag).replace(/%tag%/gi, user.tag),
+    authorText?.replace(/%tag%/gi, user.tag) ?? user.tag,
     user.displayAvatarURL({ dynamic: true })
   )
 }
