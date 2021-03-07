55b5235467adae857615afd0cca8fbbed69e6106
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-03-07 12:42:55 -0500

    avatar command now handles users with no avatar

--- commands/util/avatar.js
+++ commands/util/avatar.js
@@ -8,7 +8,7 @@ exports.run = async (client, message, args) => {
   const emb = new Discord.MessageEmbed()
     .setColor(config.main.colours.success)
     .setDescription("Here you go!")
-    .setImage(user.avatarURL({ dynamic: true, size: 2048 }))
+    .setImage(user.displayAvatarURL({ dynamic: true, size: 2048 }))
   embedder.addAuthor(emb, user, "%tag%'s Avatar")
 
   message.channel.send(emb)
