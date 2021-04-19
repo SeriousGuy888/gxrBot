bd7a915be924624d05de987afe5cf46fdddc8d91
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-04-19 17:35:09 -0400

    Update cheezsurv.js

--- commands/fun/cheezsurv.js
+++ commands/fun/cheezsurv.js
@@ -11,18 +11,29 @@ exports.run = async (client, message, args) => {
   embedder.addAuthor(emb, message.author)
     .setColor("#ad23ad")
     .setTitle("CheezSurv4")
-
-
-  const iconBase64 = responseData.icon
-  const imageStream = Buffer.from(iconBase64?.split(",")[1], "base64")
-  const attachment = new Discord.MessageAttachment(imageStream, "icon.png")
-
-  emb
-    .attachFiles(attachment)
-    .setThumbnail("attachment://icon.png")
-    .setDescription(responseData.motd.clean.join("\n") || "`No MOTD`")
-    .addField("Players", `${responseData.players.online}/${responseData.players.max}`)
     .setFooter("graph coming soon????????")
+
+
+  let attachment
+  const iconBase64 = responseData.icon
+  if(iconBase64) {
+    const imageStream = Buffer.from(iconBase64.split(",")[1], "base64")
+    attachment = new Discord.MessageAttachment(imageStream, "icon.png")
+  }
+  if(attachment) {
+    emb
+      .attachFiles(attachment)
+      .setThumbnail("attachment://icon.png")
+  }
+
+  if(responseData.online) {
+    emb
+      .setDescription(responseData.motd.clean.join("\n") || "`No MOTD`")
+      .addField("Players", `${responseData.players.online}/${responseData.players.max}`)
+  }
+  else {
+    emb.setDescription("minehut's dukcing deceased lol")
+  }
 
 
   message.channel.send(emb)
