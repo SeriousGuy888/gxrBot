77bd04de4330f8c7863f281fc3d74a66b7504c4f
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-05-20 14:44:43 -0400

    update status

--- events/ready.js
+++ events/ready.js
@@ -20,7 +20,7 @@ module.exports = async (client, message) => {
   logger.log(`${config.main.botNames.lowerCamelCase} successfully loaded ${process.env.DEV_MODE ? "in dev mode" : ""}`)
 
   client.user.setPresence({ status: "online" })
-  client.user.setActivity(`affection uncrat beas!`, { type: "PLAYING" })
+  client.user.setActivity(`affektion beas unkrat!`, { type: "PLAYING" })
   
   
   schedule.scheduleJob("0 * * * *", () => {
