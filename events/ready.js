4d7fddcb95cbb7b7d533a9dbdf5d1fa82762cfeb
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-05-23 00:05:16 -0400

    remove mikrosoft ðing

--- events/ready.js
+++ events/ready.js
@@ -54,14 +54,14 @@ module.exports = async (client, message) => {
     minecraftPinger.recordMinehut("cheezsurv4", "cheezsurv4")
   })
 
-  schedule.scheduleJob("0 0 * * *", () => {
-    const emb = new Discord.MessageEmbed()
-      .setColor("#23a1b1")
-      .setTitle("<:microsoft:829801007764471808> This is Microsoft")
-      .setDescription("Feed me moolah immediately or your :computer: will be :bricks:ed.")
-    messenger.dm("427925505581383721", emb) // liaosmdk
-    messenger.dm("393590581000929281", emb) // jajspoder
-  })
+  // schedule.scheduleJob("0 0 * * *", () => {
+  //   const emb = new Discord.MessageEmbed()
+  //     .setColor("#23a1b1")
+  //     .setTitle("<:microsoft:829801007764471808> This is Microsoft")
+  //     .setDescription("Feed me moolah immediately or your :computer: will be :bricks:ed.")
+  //   messenger.dm("427925505581383721", emb) // liaosmdk
+  //   messenger.dm("393590581000929281", emb) // jajspoder
+  // })
 
   schedule.scheduleJob("0 */6 * * *", () => {
     const emb = new Discord.MessageEmbed()
