2b8204710872d16f8455838f45a5a81770b25c7e
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-05-27 00:35:32 -0400

    the same part of a string can no longer replaced twice in preserveCaseReplace function

--- functions/preserveCaseReplace.js
+++ functions/preserveCaseReplace.js
@@ -1,17 +1,28 @@
+/*
+  https://en.wikipedia.org/wiki/Private_Use_Areas
+  
+  Unassigned Unicode characters are used to temporarily denote
+  the places which need to be replaced so that the same place
+  is not replaced twice.
+*/
+
 module.exports = (original, replaceRegex, replaceWith, duckDiacritics) => {
   let replacementResult = original
   let diacriticsRemoved = duckDiacritics ? original.normalize("NFD").replace(/[\u0300-\u036f]/g, "") : original
   let occurences = diacriticsRemoved.match(replaceRegex)
   
+  let replacementKey = {}
+
   for(let i in occurences) {
-    let resWithPreservedCase = ""
+    let replacedOccurence = ""
     let letterRatio = occurences[i].length / replaceWith.length
+
     for(let j = 0; j < replaceWith.length; j++) {
       let censorChar = replaceWith.charAt(j)
       let originalChar = occurences[i].charAt(Math.floor(j * letterRatio))
 
-      if(originalChar.match(/[A-Z]/)) resWithPreservedCase += censorChar.toUpperCase()
-      else                            resWithPreservedCase += censorChar.toLowerCase()
+      if(originalChar.match(/[A-Z]/)) replacedOccurence += censorChar.toUpperCase()
+      else                            replacedOccurence += censorChar.toLowerCase()
     }
     
     if(duckDiacritics) {
@@ -19,7 +30,18 @@ module.exports = (original, replaceRegex, replaceWith, duckDiacritics) => {
         .normalize("NFD")
         .replace(/[\u0300-\u036f]/g, "")
     }
-    replacementResult = replacementResult.replace(occurences[i], resWithPreservedCase)
+
+    
+    const charCode = 0xe000 + i
+    const char = String.fromCharCode(charCode)
+
+    replacementKey[char] = replacedOccurence
+    replacementResult = replacementResult.replace(occurences[i], char)
   }
+
+  for(let char in replacementKey) {
+    replacementResult = replacementResult.replace(char, replacementKey[char])
+  }
+
   return replacementResult
 }
