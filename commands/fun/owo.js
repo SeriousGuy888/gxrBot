e387876bd696ac7bcdacd3dc044772e0cdf12660
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-05-27 00:38:55 -0400

    added basic owo command

---
+++ commands/fun/owo.js
@@ -0,0 +1,22 @@
+exports.run = async (client, message, args) => {
+  const index = require("../../index.js")
+  const { config, Discord } = index
+  const { preserveCaseReplace } = client.functions
+
+  if(!args[0]) return
+
+  const filters = {
+    "ow": "owo",
+    "u": "uwu",
+    "r": "w",
+  }
+
+  const originalContent = args.join(" ")
+  let newContent = originalContent
+
+  for(let loopFilter in filters) {
+    newContent = preserveCaseReplace(newContent, new RegExp(loopFilter, "gi"), filters[loopFilter], false)
+  }
+
+  message.channel.send(newContent)
+}
