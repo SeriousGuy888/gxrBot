9f1041fc32709aacf8b0c6c608e52d25e8aaae4d
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-05-29 11:23:07 -0400

    Revert "the same part of a string can no longer replaced twice in preserveCaseReplace function"
    This reverts commit 2b8204710872d16f8455838f45a5a81770b25c7e.

--- functions/preserveCaseReplace.js
+++ functions/preserveCaseReplace.js
@@ -1,28 +1,17 @@
-/*
-  https://en.wikipedia.org/wiki/Private_Use_Areas
-  
-  Unassigned Unicode characters are used to temporarily denote
-  the places which need to be replaced so that the same place
-  is not replaced twice.
-*/
-
 module.exports = (original, replaceRegex, replaceWith, duckDiacritics) => {
   let replacementResult = original
   let diacriticsRemoved = duckDiacritics ? original.normalize("NFD").replace(/[\u0300-\u036f]/g, "") : original
   let occurences = diacriticsRemoved.match(replaceRegex)
   
-  let replacementKey = {}
-
   for(let i in occurences) {
-    let replacedOccurence = ""
+    let resWithPreservedCase = ""
     let letterRatio = occurences[i].length / replaceWith.length
-
     for(let j = 0; j < replaceWith.length; j++) {
       let censorChar = replaceWith.charAt(j)
       let originalChar = occurences[i].charAt(Math.floor(j * letterRatio))
 
-      if(originalChar.match(/[A-Z]/)) replacedOccurence += censorChar.toUpperCase()
-      else                            replacedOccurence += censorChar.toLowerCase()
+      if(originalChar.match(/[A-Z]/)) resWithPreservedCase += censorChar.toUpperCase()
+      else                            resWithPreservedCase += censorChar.toLowerCase()
     }
     
     if(duckDiacritics) {
@@ -30,18 +19,7 @@ module.exports = (original, replaceRegex, replaceWith, duckDiacritics) => {
         .normalize("NFD")
         .replace(/[\u0300-\u036f]/g, "")
     }
-
-    
-    const charCode = 0xe000 + i
-    const char = String.fromCharCode(charCode)
-
-    replacementKey[char] = replacedOccurence
-    replacementResult = replacementResult.replace(occurences[i], char)
+    replacementResult = replacementResult.replace(occurences[i], resWithPreservedCase)
   }
-
-  for(let char in replacementKey) {
-    replacementResult = replacementResult.replace(char, replacementKey[char])
-  }
-
   return replacementResult
 }
