5fa0af97e6dd1a5ad76edf6d09455cab02e47436
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-05-31 15:53:22 -0400

    scheduled tasks moved to its own file in functions/

--- events/ready.js
+++ events/ready.js
@@ -1,18 +1,8 @@
-module.exports = async (client, message) => {
+module.exports = async (client) => {
   const index = require("../index.js")
-  const { config, Discord, schedule } = index
-  const {
-    badger,
-    banker,
-    karmanator,
-    logger,
-    messenger,
-    minecraftPinger,
-    preferencer,
-    guildPreferencer,
-    statTracker
-  } = client.util
-  const { awaitOrders } = client.functions
+  const { config } = index
+  const { logger } = client.util
+  const { awaitOrders, scheduleTasks } = client.functions
 
   awaitOrders()
   logger.log(`${config.main.botNames.lowerCamelCase} successfully loaded ${process.env.DEV_MODE ? "in dev mode" : ""}`)
@@ -20,30 +10,5 @@ module.exports = async (client, message) => {
   client.user.setPresence({ status: "online" })
   client.user.setActivity(`Jasper for vcdeAict8ricr!`, { type: "PLAYING" })
   
-  
-  schedule.scheduleJob("0 * * * *", () => {
-    client.util.logger.uploadLogs("Hourly automatic log upload")
-  })
-
-  setInterval(() => {
-    karmanator.update()
-    statTracker.update()
-    preferencer.update()
-    guildPreferencer.update()
-    badger.updateBadges()
-    banker.updateBalances()
-    banker.updateInventories()
-  }, 5 * 60 * 1000)
-
-  setInterval(() => minecraftPinger.update(), 30 * 60 * 1000)
-  setTimeout(async () => index.propaganda(client), 1000)
-  schedule.scheduleJob("*/5 * * * *", async () => minecraftPinger.recordMinehut("cheezsurv4", "cheezsurv4"))
-
-  schedule.scheduleJob("0 */6 * * *", () => {
-    const emb = new Discord.MessageEmbed()
-      .setColor("#c32323")
-      .setTitle("fact pls")
-      .setDescription("<#735708848333258822>")
-    messenger.dm("414938441546203136", emb)
-  })
+  scheduleTasks(client)
 }
---
+++ functions/scheduleTasks.js
@@ -0,0 +1,40 @@
+module.exports = (client) => {
+  const index = require("../index.js")
+  const { Discord, schedule } = index
+  const {
+    badger,
+    banker,
+    karmanator,
+    messenger,
+    minecraftPinger,
+    preferencer,
+    guildPreferencer,
+    statTracker
+  } = client.util
+  
+  schedule.scheduleJob("0 * * * *", () => {
+    client.util.logger.uploadLogs("Hourly automatic log upload")
+  })
+
+  setInterval(() => {
+    karmanator.update()
+    statTracker.update()
+    preferencer.update()
+    guildPreferencer.update()
+    badger.updateBadges()
+    banker.updateBalances()
+    banker.updateInventories()
+  }, 5 * 60 * 1000)
+
+  setInterval(() => minecraftPinger.update(), 30 * 60 * 1000)
+  setTimeout(async () => index.propaganda(client), 1000)
+  schedule.scheduleJob("*/5 * * * *", async () => minecraftPinger.recordMinehut("cheezsurv4", "cheezsurv4"))
+
+  schedule.scheduleJob("0 */6 * * *", () => {
+    const emb = new Discord.MessageEmbed()
+      .setColor("#c32323")
+      .setTitle("fact pls")
+      .setDescription("<#735708848333258822>")
+    messenger.dm("414938441546203136", emb)
+  })
+}
