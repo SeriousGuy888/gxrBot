7211610d626cbc575acbfdb6390eb5144024738e
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-06-10 16:41:02 -0400

    remove liam_leagues form config

--- config/coopchannels/_coopchannels.js
+++ config/coopchannels/_coopchannels.js
@@ -1,4 +1,3 @@
 module.exports.counting = require("./counting.json")
 module.exports.cult = require("./cult.json")
-module.exports.liamLeagues = require("./liam_leagues.json")
 module.exports.ows = require("./ows.json")
