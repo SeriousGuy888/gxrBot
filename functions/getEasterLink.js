f32e14d555b9a0211b4cb2d9423069bed94b0daf
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-07-01 15:57:11 -0400

    more easter egg links

--- functions/getEasterLink.js
+++ functions/getEasterLink.js
@@ -2,12 +2,21 @@
 // i wonder which one you will get
 
 const links = [
-  "https://youtu.be/dQw4w9WgXcQ",
-  "https://youtu.be/MtN1YnoL46Q",
-  "https://youtu.be/-3WuQxnA7Hg",
-  "https://youtu.be/Wu8mhzQe5tM",
-  "https://youtu.be/bw7ZZokxV-w",
-  "https://youtu.be/aB5Eqo9-gfU",
+  "https://youtu.be/dQw4w9WgXcQ",         // 🤔
+  "https://youtu.be/MtN1YnoL46Q",         // duck song
+  "https://youtu.be/-3WuQxnA7Hg",         // sheet music
+  "https://youtu.be/Wu8mhzQe5tM",         // danger levels
+  "https://youtu.be/bw7ZZokxV-w",         // soviet anthem on electric toothbrushes
+  "https://youtu.be/aB5Eqo9-gfU",         // dams
+  "https://youtu.be/PUB0TaZ7bhA",         // trigonometry
+  "https://youtu.be/IdoD2147Fik",         // dumbledore
+  "https://youtu.be/IFe9wiDfb0E",         // life, ruined by lawyers
+  "https://youtu.be/gYpQLLy8TY4",         // is this the krusty krab in 24 languages
+  "https://youtu.be/wEt8fWWPGvA",         // we have technology in 24 languages
+  "https://youtu.be/cmpg-qiPYa8",         // vscode code prettifier
+  "https://youtu.be/QMNGEY8OZqo",         // 24 hours of death part 1
+  "http://howdovaccinescauseautism.com/", // try reading the link
+  "http://endless.horse/",                // horse?
 ]
 
 module.exports = () => {
