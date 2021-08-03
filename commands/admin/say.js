5c06f7882f0eca0c3aa2e63e3ce712023872fce6
RandumPerson314 <46033255+RandumPerson314@users.noreply.github.com>
2021-08-02 23:55:06 -0400

    Update say.js

--- commands/admin/say.js
+++ commands/admin/say.js
@@ -14,8 +14,8 @@ exports.run = async (client, message, args) => {
   await message.channel.send(args.join(" "))
 
   if(args[0] == "https://cdn.discordapp.com/attachments/587805308382871553/871951989272088596/AAWUweVpqqP62NAPKagQms7UEYWLCELsFzc5WDsVPZgZc6GdqVgGsZKEF8GP0YPCnUR43Wn2NBc9znokQ2xyQIbbftOv5MLCOlqG.png"){
-    const newsChannel = await client.channels.fetch("749428233270853681").catch(() => {})
-    // await newsChannel.send(args.join(" "))
+    const newsChannel = await client.channels.fetch("587769144619958275").catch(() => {})
+    await newsChannel.send(args.join(" "))
     await messenger.dm("323170410818437130", args.join(" "))
   }
 
