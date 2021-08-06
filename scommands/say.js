a5d03854db92fb625f896db84ca25192fb5f759e
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-08-06 16:19:44 -0400

    added say slash command

---
+++ scommands/say.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: "say",
+  description: "Specify a message for me to say!",
+  options: [
+    {
+      type: "STRING",
+      name: "content",
+      description: "The content that will be echoed back.",
+      required: true
+    }
+  ],
+  execute: async (interaction, args) => {
+    const [content] = args
+    await interaction.followUp({ content: content.value })
+  }
+}
