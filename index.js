85fef96f3ed2f842449513dae481aac16448c86f
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-08-06 12:25:54 -0400

    fixed intents

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const fs = require("fs")
 const path = require("path")
 
 const Discord = require("discord.js")
-const client = new Discord.Client({
+const { Client, Intents } = Discord
+const client = new Client({
   partials: [ // discord client with partials
     "USER",
     "CHANNEL",
@@ -12,7 +13,23 @@ const client = new Discord.Client({
     "GUILD_MEMBER",
     "REACTION"
   ],
-  intents: new Discord.Intents(Discord.Intents.ALL)
+  intents: [ // i guess Intents.ALL doesnt exist
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_BANS,
+    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    Intents.FLAGS.GUILD_INTEGRATIONS,
+    Intents.FLAGS.GUILD_WEBHOOKS,
+    Intents.FLAGS.GUILD_INVITES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_PRESENCES,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_MESSAGE_TYPING,
+    Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+  ]
 })
 
 
