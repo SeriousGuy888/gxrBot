c321b821f7a868635ecde5e6ba3158d938297bcc
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-08-06 16:33:42 -0400

    testing slash command descriptions should be fixed

--- functions/deploySlashCommands.js
+++ functions/deploySlashCommands.js
@@ -4,15 +4,16 @@ const settings = config.main
 module.exports = async (client) => {
   const slashCommandData = [...client.scommands.values()]
 
-  const testGuildCommandData = [...client.scommands.values()]
-  testGuildCommandData.push({ name: "borken" })
-  testGuildCommandData.map(e => e.description = "🧪 Test guild slash command.")
 
-
-  client.guilds.cache.get(settings.testGuildId)?.commands.set(slashCommandData)
-    .then(() => console.log("🎉 Deployed slash commands to test server!"))
-    .catch(err => console.error(`Error deploying slash commands to test server.\n`, err))
   client.application?.commands.set(slashCommandData)
     .then(() => console.log("🎉 Deployed slash commands globally!"))
     .catch(err => console.error(`Error deploying slash commands globally.\n`, err))
+
+
+    
+  slashCommandData.push({ name: "borken" })
+  slashCommandData.map(e => e.description = "🧪 Test guild slash command.")
+  client.guilds.cache.get(settings.testGuildId)?.commands.set(slashCommandData)
+    .then(() => console.log("🎉 Deployed slash commands to test server!"))
+    .catch(err => console.error(`Error deploying slash commands to test server.\n`, err))
 }
