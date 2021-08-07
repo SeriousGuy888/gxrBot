8fe973e5dce74bf214e540b975dfe0494517b15c
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-08-07 17:27:12 -0400

    add anonymous option to say command

--- scommands/say.js
+++ scommands/say.js
@@ -7,11 +7,23 @@ module.exports = {
       name: "content",
       description: "The content that will be echoed back.",
       required: true
+    },
+    {
+      type: "BOOLEAN",
+      name: "anonymous",
+      description: "The output will not show that you made me say this."
     }
   ],
   defer: false,
   execute: async (interaction, options) => {
-    const content = options.getString("content")
-    await interaction.reply({ content: content.slice(0, 2000) })
+    const content = options.getString("content").slice(0, 2000)
+    const anonymous = !!options.getBoolean("anonymous")
+
+    if(anonymous) {
+      await interaction.channel.send({ content })
+    }
+    else {
+      await interaction.reply({ content })
+    }
   }
 }
