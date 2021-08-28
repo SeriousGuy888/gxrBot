06c37d89b9323cc8331f15ad0d140a5882998642
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-08-28 19:09:44 -0400

    reenabled birthday command

--- commands/util/birthday.js
+++ commands/util/birthday.js
@@ -3,10 +3,10 @@ exports.run = async (client, message, args) => {
   const { config, Discord } = index
   const { embedder } = client.util
 
-  if(message.guild && message.guild.id !== config.main.guild.id) {
-    message.channel.send(`You cannot use this command outside of \`${config.main.guild.name}\`!`)
-    return
-  }
+  // if(message.guild && message.guild.id !== config.main.guild.id) {
+  //   message.channel.send({ content: `You cannot use this command outside of \`${config.main.guild.name}\`!` })
+  //   return
+  // }
   if(!args[1]) {
     this.help(client, message, args)
     return
@@ -22,12 +22,12 @@ exports.run = async (client, message, args) => {
   const day =   parseInt(args[1])
 
   if(!month || !day) {
-    message.channel.send("specify valid numbers pls. eg: `birthday 4 20` for april 20")
+    message.channel.send({ content: "specify valid numbers pls. eg: `birthday 4 20` for april 20" })
     return
   }
 
   if(month < 1 || month > 12) {
-    message.channel.send("that is not a monthdfjh. eg: `birthday 4 20` for april 20")
+    message.channel.send({ content: "that is not a monthdfjh. eg: `birthday 4 20` for april 20" })
     return
   }
 
@@ -37,12 +37,12 @@ exports.run = async (client, message, args) => {
   if(monthLengths.february.includes(month)) maxDay = 29
 
   if(day < 1 || day > maxDay) {
-    message.channel.send("invalid day for the month!")
+    message.channel.send({ content: "invalid day for the month!" })
     return
   }
 
   
-  const msg = await message.channel.send(":thinking: Hmmm")
+  const msg = await message.channel.send({ content: ":thinking: Hmmm" })
   const date = new Date(1970, month - 1, day)
   const dateStr = date.toLocaleString("default", { month: "long", day: "numeric" })
   // const isoDateString = `-${month.toString().padStart(2, "0")}-${day.toString().padStart(2, "0")}`
@@ -55,30 +55,23 @@ exports.run = async (client, message, args) => {
     .setDescription(`Here is what I got: \`${dateStr}\`. Is this correct?`)
 
   const emoji = "✅"
-  await msg.edit(confirmEmbed)
+  await msg.edit({ embeds: [confirmEmbed] })
   msg.react(emoji)
 
   const filter = (reaction, reactor) => (reaction.emoji.name === emoji) && (reactor.id === message.author.id)
-  msg.awaitReactions(filter, { max: 1, time: 15000 })
-    .then(async collected => {
-      const responseEmbed = new Discord.MessageEmbed()
-      embedder.addAuthor(responseEmbed, message.author)
-        .setColor(config.main.colours.success)
-        .setTitle(`✅ Check Mark.`)
-        .setDescription(`hoang will be notified of your birthday`)
-    
-
-      const secretChannel = await client.channels.fetch("767186885893292042").catch(() => {})
-
-      secretChannel.send(`${message.author} says their birthday is \`${dateStr}\``)
-        .then(() => msg.edit(responseEmbed))
-        .catch(() => message.channel.send("hoang cant be notified because the channel doesnt exist or something is broken or osmethging. tell hoang yourself i guess"))
-    })
-    .catch(err => {
-      console.log(err)
-      confirmEmbed.setColor(config.main.colours.error)
-      msg.edit("No confirmation D:", { embed: confirmEmbed })
-    })
+  const collector = msg.createReactionCollector({ max: 1, time: 15000, filter })
+  collector.on("collect", () => {
+    const responseEmbed = new Discord.MessageEmbed()
+    embedder.addAuthor(responseEmbed, message.author)
+      .setColor(config.main.colours.success)
+      .setTitle(`✅ Check Mark.`)
+      .setDescription(`hoang will be notified of your birthday`)
+  
+    const secretChannel = await client.channels.fetch("767186885893292042").catch(() => {})
+    secretChannel.send(`${message.author} says their birthday is \`${dateStr}\``)
+      .then(() => msg.edit({ embeds: [responseEmbed] }))
+      .catch(() => message.channel.send({ content: "hoang cant be notified because the channel doesnt exist or something is broken or osmethging. tell hoang yourself i guess" }))
+  })
 }
 
 exports.help = async (client, message, args) => {
@@ -100,9 +93,7 @@ exports.help = async (client, message, args) => {
     ].join("\n"),
   })
   
-  message.channel.send(embed)
+  message.channel.send({ embeds: [embed] })
 }
 
 exports.cooldown = 5
-
-exports.disabled = "temp disabled during discord.js v13 update"
