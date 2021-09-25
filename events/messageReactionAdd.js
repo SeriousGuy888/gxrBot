4b6a8e73092f2b6cac61debbcb332f90a3a881b0
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-09-25 00:19:43 -0400

    remove useless message fetch on reaction events

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,9 +1,4 @@
 module.exports = async (client, reaction, user) => {
   const { karmanator } = client.util
-
-  let message = reaction.message
-  if(reaction.message.partial)
-    message = await reaction.message.fetch()
-
   karmanator.countVote(reaction, user, false)
 }
